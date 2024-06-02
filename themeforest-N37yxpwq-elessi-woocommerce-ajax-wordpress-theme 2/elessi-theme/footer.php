57ca1ed06701023fa36e6083f465547c6c218477
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-06-03 00:51:12 +0500

    2

---
+++ themeforest-N37yxpwq-elessi-woocommerce-ajax-wordpress-theme 2/elessi-theme/footer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package nasatheme
+ */
+?>
+</main>
+<!-- End Main Content Site -->
+
+<?php
+/**
+ *  Footer Site 
+ */
+do_action('nasa_footer_layout_style'); ?>
+
+</div>
+<!-- End Wrapper Site -->
+
+<?php
+wp_footer();
+echo '</body></html>';
