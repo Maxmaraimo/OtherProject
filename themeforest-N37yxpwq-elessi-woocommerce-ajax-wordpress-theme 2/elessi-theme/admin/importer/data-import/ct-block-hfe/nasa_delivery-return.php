c929cd41fba3809896095f227aa4e024cedd492f
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-06-13 00:45:42 +0500

    6

---
+++ themeforest-N37yxpwq-elessi-woocommerce-ajax-wordpress-theme 2/elessi-theme/admin/importer/data-import/ct-block-hfe/nasa_delivery-return.php
@@ -0,0 +1,17 @@
+<?php
+function nasa_hfe_delivery_return() {
+    return array(
+        'post' => array(
+            'post_title' => 'HFE Delivery & Return',
+            'post_name' => 'hfe-delivery-return'
+        ),
+        
+        'post_meta' => array(
+            '_elementor_data' => '[{"id":"148ecd6","elType":"container","settings":[],"elements":[{"id":"1663978","elType":"widget","settings":{"editor":"<h2 class=\"fs-25\">Delivery<\/h2>\n<h5>Store delivery <span style=\"font-size: 14px; color: #00ca8d;\"> FREE<\/span><\/h5>\n<p>1-3 working days<\/p>\n<h5>Home or collection point from \u00a335.00<span style=\"font-size: 14px; color: #00ca8d;\"> FREE<\/span><\/h5>\n<p>On all your orders for home or collection point delivery<\/p>\n<h2 class=\"fs-25\">Returns<\/h2>\n<h5>Return<\/h5>\n<p>We will accept exchanges and returns of unworn and unwashed garments within 30 days of the date of purchase (14 days during the sales period).<\/p>\n<h5>Returns in store <span style=\"font-size: 14px; color: #00ca8d;\">FREE<\/span><\/h5>\n<p>Your return will usually be processed within a week to a week and a half. We\u2019ll send you a Return Notification email to notify you once the return has been completed.\nPlease allow 1-3 business days for refunds to be received to the original form of payment once the return has been processed.<\/p>"},"elements":[],"widgetType":"text-editor"}],"isInner":false}]',
+            '_elementor_css' => '',
+            // 'ehf_template_type' => 'custom'
+        ),
+
+        'css' => ''
+    );
+}
