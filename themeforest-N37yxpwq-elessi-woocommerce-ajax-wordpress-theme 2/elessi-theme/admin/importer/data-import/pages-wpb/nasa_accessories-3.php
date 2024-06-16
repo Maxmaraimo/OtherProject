36ee0c2389b6462168efe7f3093f13e667646999
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-06-17 01:30:42 +0500

    19

---
+++ themeforest-N37yxpwq-elessi-woocommerce-ajax-wordpress-theme 2/elessi-theme/admin/importer/data-import/pages-wpb/nasa_accessories-3.php
@@ -0,0 +1,85 @@
+<?php
+function nasa_wpb_accessories_3() {
+    $imgs_1 = elessi_import_upload('/wp-content/uploads/2021/10/h3-banner1.jpg', '3098', array(
+        'post_title' => 'h3-banner1',
+        'post_name' => 'h3-banner1',
+    ));
+    
+    $imgs_2 = elessi_import_upload('/wp-content/uploads/2021/10/h3-banner2.jpg', '3098', array(
+        'post_title' => 'h3-banner2',
+        'post_name' => 'h3-banner2',
+    ));
+    
+    $imgs_3 = elessi_import_upload('/wp-content/uploads/2021/10/h3-banner4.jpg', '3129', array(
+        'post_title' => 'h3-banner4',
+        'post_name' => 'h3-banner4',
+    ));
+    
+    $imgs_4 = elessi_import_upload('/wp-content/uploads/2021/10/h3-banner5.jpg', '3129', array(
+        'post_title' => 'h3-banner5',
+        'post_name' => 'h3-banner5',
+    ));
+    
+    return array(
+        'post' => array(
+            'post_title' => 'WPB Accessories V3',
+            'post_name' => 'wpb-accessories-v3',
+            'post_content' => '[vc_row fullwidth="1" el_class="margin-bottom-50"][vc_column][nasa_rev_slider alias="slider-tech-accessories-v3"][/vc_column][/vc_row][vc_row el_class="margin-bottom-30"][vc_column][nasa_products_special_deal limit="6" style="for_time-2" title_align="1" date_sc="2024-12-30" columns_number="5" columns_number_small="2" columns_number_tablet="3" arrows="1" auto_slide="false" title="What we offer" el_class="margin-bottom-50" desc_shortcode="Find great deal today, as well as those coming soon. Sale price valid for one day only."][/vc_column][/vc_row][vc_row el_class="margin-bottom-40"][vc_column width="1/2"][nasa_banner valign="middle" banner_responsive="no" effect_text="fadeInLeft" hover="zoom" img_src="' . $imgs_1 . '"]
+<h5>iPhone Case</h5>
+<h6 style="color: #a9a9a9; text-decoration: underline; font-weight: 100 !important;">Discover Now</h6>
+[/nasa_banner][/vc_column][vc_column width="1/2"][nasa_banner valign="middle" banner_responsive="no" hover="zoom" img_src="' . $imgs_2 . '"]
+<h5>Superior sound</h5>
+<h6 class="fs-20" style="font-weight: 100 !important;"><span style="color: #ff0000;">25%</span> <span style="color: #a9a9a9;">Off</span></h6>
+<a class="button margin-top-10 hide-for-small fs-15 primary-color" style="border-radius: 5px; color: #fff; border: none; font-weight: 600 !important; text-transform: capitalize; letter-spacing: 0;" title="Shop now" href="#">Shop Now</a>[/nasa_banner][/vc_column][/vc_row][vc_row el_class="margin-bottom-35"][vc_column][vc_tta_tabs alignment="right" tabs_display_type="2d-has-bg" tabs_bg_color="#f4f4f4" title="Wireless Headphones" el_class="letter-spacing-none"][vc_tta_section title="New Arrivals" tab_id="1557562317520-c6a3e89e-e515"][nasa_products type="on_sale" style="carousel" shop_url="0" arrows="0" number="6" columns_number="5" columns_number_tablet="3"][/vc_tta_section][vc_tta_section title="Featured" tab_id="1557562317539-34fb2fe3-faf4"][nasa_products type="on_sale" style="carousel" shop_url="0" arrows="0" number="6" columns_number="5" columns_number_tablet="3"][/vc_tta_section][vc_tta_section title="Popular" tab_id="1557562412459-ebc2e257-66b8"][nasa_products style="carousel" shop_url="0" arrows="0" number="6" columns_number="5" columns_number_tablet="3"][/vc_tta_section][vc_tta_section title="On Sale" tab_id="1557562413638-4289230d-f983"][nasa_products type="on_sale" style="carousel" shop_url="0" arrows="0" number="6" columns_number="5" columns_number_tablet="3"][/vc_tta_section][/vc_tta_tabs][/vc_column][/vc_row][vc_row el_class="margin-bottom-35"][vc_column][vc_tta_tabs alignment="right" tabs_display_type="2d-has-bg" tabs_bg_color="#f4f4f4" title="Phone Case" el_class="letter-spacing-none"][vc_tta_section title="New Arrivals" tab_id="1557562979840-b7044cf6-d4aa"][nasa_products type="featured_product" style="carousel" shop_url="0" arrows="0" number="6" columns_number="5" columns_number_tablet="3" min_height=""][/vc_tta_section][vc_tta_section title="Featured" tab_id="1557562979893-76dadca0-6e3e"][nasa_products style="carousel" shop_url="0" arrows="0" number="6" columns_number="5" columns_number_tablet="3"][/vc_tta_section][vc_tta_section title="Popular" tab_id="1557562979950-0cffb725-bc44"][nasa_products type="deals" style="carousel" shop_url="0" arrows="0" number="6" columns_number="5" columns_number_tablet="3"][/vc_tta_section][vc_tta_section title="On Sale" tab_id="1557562979998-723ede4d-55d0"][nasa_products type="on_sale" style="carousel" shop_url="0" arrows="0" number="6" columns_number="5" columns_number_tablet="3"][/vc_tta_section][/vc_tta_tabs][/vc_column][/vc_row][vc_row el_class="margin-bottom-30"][vc_column][vc_tta_tabs alignment="right" tabs_display_type="2d-has-bg" tabs_bg_color="#f4f4f4" title="Speaker" el_class="letter-spacing-none"][vc_tta_section title="New Arrivals" tab_id="1557562973337-f8b40dc9-6081"][nasa_products style="carousel" shop_url="0" arrows="0" number="6" columns_number="5" columns_number_tablet="3" min_height=""][/vc_tta_section][vc_tta_section title="Featured" tab_id="1557562973395-90fdc6b4-7f92"][nasa_products type="on_sale" style="carousel" shop_url="0" arrows="0" number="6" columns_number="5" columns_number_tablet="3"][/vc_tta_section][vc_tta_section title="Popular" tab_id="1557562973451-fcd5f590-10b2"][nasa_products type="stock_desc" style="carousel" shop_url="0" arrows="0" number="6" columns_number="5" columns_number_tablet="3"][/vc_tta_section][vc_tta_section title="On Sale" tab_id="1557562973510-e4436084-0490"][nasa_products type="on_sale" style="carousel" shop_url="0" arrows="0" number="6" columns_number="5" columns_number_tablet="3"][/vc_tta_section][/vc_tta_tabs][/vc_column][/vc_row][vc_row el_class="margin-bottom-40"][vc_column width="1/2"][nasa_banner valign="middle" banner_responsive="no" hover="zoom" img_src="' . $imgs_3 . '"]
+<h5 style="font-weight: bold;">Drone Radio
+Controller<span style="font-weight: 300;"> Accessories</span></h5>
+<h6 class="fs-18" style="color: #666; font-weight: 100 !important;">Just   <span class="fs-18" style="font-weight: 500; color: #690;">$139.99</span></h6>
+<a class="button margin-top-20 hide-for-small fs-15 primary-color" style="border-radius: 5px; color: #fff; border: none; font-weight: 600 !important; text-transform: capitalize; letter-spacing: 0;" title="Shop now" href="#">Shop Now</a>[/nasa_banner][/vc_column][vc_column width="1/2"][nasa_banner banner_responsive="no" hover="zoom" img_src="' . $imgs_4 . '"]
+<h5 style="font-weight: bold;">Virtual Reality
+Headset<span style="font-weight: 300;"> Accessories</span></h5>
+<h6 class="fs-18" style="color: #666; font-weight: 100 !important;">Just   <span class="fs-18" style="font-weight: 500; color: #690;">$69.99</span></h6>
+<a class="button margin-top-20 hide-for-small fs-15 primary-color" style="border-radius: 5px; color: #fff; border: none; font-weight: 600 !important; text-transform: capitalize; letter-spacing: 0;" title="Shop now" href="#">Shop Now</a>[/nasa_banner][/vc_column][/vc_row][vc_row el_class="margin-bottom-40"][vc_column][vc_tta_tabs alignment="right" tabs_display_type="2d-has-bg" tabs_bg_color="#f4f4f4" title="New Arrivals" el_class="animation-none letter-spacing-none"][vc_tta_section title="Cables" tab_id="1557563197385-464d0093-f782" el_class="padding-top-25"][nasa_products style="list" pos_nav="left" number="6" columns_number="3" columns_number_tablet="2" columns_number_small="1" min_height=""][/vc_tta_section][vc_tta_section title="Protection" tab_id="1557563197312-b1096055-92ac" el_class="padding-top-25"][nasa_products type="on_sale" style="list" pos_nav="left" number="6" columns_number="3" columns_number_tablet="2" columns_number_small="1" min_height=""][/vc_tta_section][vc_tta_section tab_id="1557563197233-70e51a46-654e" title="Case &amp; Cover" el_class="padding-top-25"][nasa_products type="featured_product" style="list" pos_nav="left" number="6" columns_number="3" columns_number_tablet="2" columns_number_small="1" min_height=""][/vc_tta_section][vc_tta_section title="Baterries" tab_id="1557563197451-7125212a-da0f" el_class="padding-top-25"][nasa_products type="deals" style="list" pos_nav="left" number="6" columns_number="3" columns_number_tablet="2" columns_number_small="1" min_height=""][/vc_tta_section][/vc_tta_tabs][/vc_column][/vc_row][vc_row el_class="margin-bottom-60 mobile-margin-bottom-20 nasa-service-footer"][vc_column width="1/4" offset="vc_col-xs-6" el_class="mobile-margin-bottom-30"][nasa_service_box service_blank="_blank" service_hover="buzz_effect" service_title="Free Delivery" service_desc="For all Order Over $50" service_icon="pe-7s-car"][/vc_column][vc_column width="1/4" offset="vc_col-xs-6" el_class="mobile-margin-bottom-30"][nasa_service_box service_blank="_blank" service_hover="buzz_effect" service_title="Discount" service_desc="40% Discount for member" service_icon="pe-7s-piggy"][/vc_column][vc_column width="1/4" offset="vc_col-xs-6" el_class="mobile-margin-bottom-30"][nasa_service_box service_blank="_blank" service_hover="buzz_effect" service_title="Secure payment" service_desc="Secure payment" service_icon="pe-7s-credit"][/vc_column][vc_column width="1/4" offset="vc_col-xs-6" el_class="mobile-margin-bottom-30"][nasa_service_box service_blank="_blank" service_hover="buzz_effect" service_title="Help Center" service_desc="24/7 Support System" service_icon="pe-7s-headphones"][/vc_column][/vc_row]'
+        ),
+        'post_meta' => array(
+            // '_nasa_custom_header' => '4',
+            '_nasa_vertical_menu_allways_show' => 'on',
+            // '_nasa_plus_wide_option' => '1',
+            // '_nasa_plus_wide_width' => '100',
+            // '_nasa_pri_color_flag' => 'on',
+            // '_nasa_pri_color' => '#73ad21',
+            // '_nasa_bg_color_main_menu' => '#73ad21',
+            // '_nasa_text_color_main_menu' => '#ffffff',
+            // '_nasa_bg_color_v_menu' => '#539309',
+            // '_nasa_text_color_v_menu' => '#ffffff'
+        ),
+        
+        'globals' => array(
+            'header-type' => '4',
+            
+            'fixed_nav' => '0',
+            
+            'plus_wide_width' => '100',
+            'color_primary' => '#73ad21',
+            
+            // 'fullwidth_main_menu' => '1',
+            
+            'bg_color_main_menu' => '#73ad21',
+            'text_color_main_menu' => '#ffffff',
+            
+            'bg_color_v_menu' => '#539309',
+            'text_color_v_menu' => '#ffffff',
+            
+            'footer_mode' => 'builder',
+            'footer-type' => 'footer-light-2',
+            'footer-mobile' => 'footer-mobile',
+            
+            'product_detail_layout' => 'modern-1',
+            'tab_style_info' => 'scroll-down',
+            'loop_layout_buttons' => 'modern-3',
+            
+            'animated_products' => 'hover-left-to-right',
+            
+            'nasa_attr_image_style' => 'square'
+        ),
+    );
+}
