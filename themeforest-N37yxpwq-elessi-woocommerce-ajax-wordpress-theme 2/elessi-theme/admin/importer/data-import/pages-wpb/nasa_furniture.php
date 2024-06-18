b6afa7eedcbc5d92aa329a9ed68a3366b8094f6a
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-06-18 12:45:39 +0500

    19

---
+++ themeforest-N37yxpwq-elessi-woocommerce-ajax-wordpress-theme 2/elessi-theme/admin/importer/data-import/pages-wpb/nasa_furniture.php
@@ -0,0 +1,96 @@
+<?php
+function nasa_wpb_furniture() {
+    $imgs_1 = elessi_import_upload('/wp-content/uploads/2019/02/furniture-sofa-banner.jpg', '3117', array(
+        'post_title' => 'furniture-sofa-banner',
+        'post_name' => 'furniture-sofa-banner',
+    ));
+    
+    $imgs_2 = elessi_import_upload('/wp-content/uploads/2019/02/furniture-lightning-banner.jpg', '3115', array(
+        'post_title' => 'furniture-lightning-banner',
+        'post_name' => 'furniture-lightning-banner',
+    ));
+    
+    $imgs_3 = elessi_import_upload('/wp-content/uploads/2019/02/furniture-clock-banner.jpg', '3115', array(
+        'post_title' => 'furniture-clock-banner',
+        'post_name' => 'furniture-clock-banner',
+    ));
+    
+    $imgs_4 = elessi_import_upload('/wp-content/uploads/2019/02/cat-banner5.jpg', '3094', array(
+        'post_title' => 'cat-banner5',
+        'post_name' => 'cat-banner5',
+    ));
+    
+    return array(
+        'post' => array(
+            'post_title' => 'WPB Furniture',
+            'post_name' => 'wpb-furniture',
+            'post_content' => '[vc_row fullwidth="1"][vc_column el_class="text-center"][nasa_pin_products_banner pin_slug="pin-products-home-furniture" bg_icon="#ffffff" txt_color="#333333" border_icon="#fed700"][/vc_column][/vc_row][vc_row el_class="margin-top-60"][vc_column][nasa_title title_text="Featured Categories" title_type="h3" font_size="xl" title_desc="The freshest and most exciting news" title_align="text-center"][/vc_column][/vc_row][vc_row el_class="desktop-margin-top-30"][vc_column width="1/2"][vc_row_inner][vc_column_inner][nasa_banner valign="bottom" hover="zoom" img_src="' . $imgs_1 . '"]
+    <div><a style="background-color: #fff; font-size: 20px; padding: 0 15px;" title="Sofas" href="#"><strong>Sofas</strong></a></div>
+    [/nasa_banner][/vc_column_inner][/vc_row_inner][vc_row_inner][vc_column_inner width="1/2"][nasa_banner valign="bottom" hover="zoom" img_src="' . $imgs_2 . '"]
+    <div><a style="background-color: #fff; font-size: 20px; padding: 0 15px;" title="Lightning" href="#"><strong>Lightning</strong></a></div>
+    [/nasa_banner][/vc_column_inner][vc_column_inner width="1/2"][nasa_banner valign="bottom" hover="fade" img_src="' . $imgs_3 . '"]
+    <div><a style="background-color: #fff; font-size: 20px; padding: 0 15px;" title="Clocks" href="#"><strong>Clocks</strong></a></div>
+    [/nasa_banner][/vc_column_inner][/vc_row_inner][/vc_column][vc_column width="1/2"][nasa_banner valign="bottom" hover="zoom" img_src="' . $imgs_4 . '"]
+    <div><a style="background-color: #fff; font-size: 20px; padding: 0 15px;" title="Chair" href="#"><strong>Chair</strong></a></div>
+    [/nasa_banner][/vc_column][/vc_row][vc_row el_class="margin-top-60"][vc_column][nasa_title title_text="Best Seller Product" title_type="h3" font_size="xl" title_align="text-center"][/vc_column][/vc_row][vc_row][vc_column][vc_tta_tabs alignment="center" tabs_display_type="2d" css=".vc_custom_1550403944446{background-color: #eeee22 !important;}" el_class="nasa-tab-primary-color"][vc_tta_section title="All" tab_id="1550311240722-2cdb1c54-ef96"][nasa_products columns_number="4" columns_number_small="1" columns_number_tablet="2"][/vc_tta_section][vc_tta_section title="Best Seller" tab_id="1550311240736-7586aefb-daf1"][nasa_products type="best_selling" columns_number="4" columns_number_small="1" columns_number_tablet="2"][/vc_tta_section][vc_tta_section title="Featured" tab_id="1550311288684-f7c002b0-60c6"][nasa_products type="featured_product" columns_number="4" columns_number_small="1" columns_number_tablet="2"][/vc_tta_section][vc_tta_section title="On Sale" tab_id="1550311289708-6556397c-37fb"][nasa_products type="on_sale" columns_number="4" columns_number_small="1" columns_number_tablet="2"][/vc_tta_section][vc_tta_section title="Deal" tab_id="1550311290709-86e146eb-efd1"][nasa_products type="deals" columns_number="4" columns_number_small="1" columns_number_tablet="2"][/vc_tta_section][/vc_tta_tabs][/vc_column][/vc_row][vc_row css=".vc_custom_1550418776704{background-color: #fed700 !important;background-position: 0 0 !important;background-repeat: repeat !important;}" el_class="mobile-margin-top-40 mobile-margin-bottom-20 desktop-margin-top-40 desktop-margin-bottom-80 padding-top-15 padding-bottom-15"][vc_column][nasa_slider bullets="false" navigation="false" autoplay="true"][vc_column_text]
+    <p class="text-center"><strong>UP TO 70% OFF THE ENTRIRE STORE! - MADE WITH LOVE by <span class="nasa-underline">Nasa studio</span></strong></p>
+    [/vc_column_text][vc_column_text]
+    <p class="text-center"><strong>UP TO 70% OFF THE ENTRIRE STORE! - MADE WITH LOVE by <span class="nasa-underline">Nasa studio</span></strong></p>
+    [/vc_column_text][/nasa_slider][/vc_column][/vc_row][vc_row][vc_column width="1/2" el_class="rtl-right"][vc_video link="https://www.youtube.com/watch?v=DmFtQrnBSe0"][/vc_column][vc_column width="1/2" el_class="desktop-padding-left-40 rtl-right rtl-desktop-padding-left-0 rtl-desktop-padding-right-40"][vc_row_inner][vc_column_inner][nasa_title title_text="About Us" title_type="h3" font_size="xl"][/vc_column_inner][/vc_row_inner][vc_row_inner][vc_column_inner][vc_column_text]
+
+    Our mission is to bring together a diverse, curated collection of beautiful furniture and homewares from around the world. I was popularised in the 1960s with the release.
+
+    [/vc_column_text][/vc_column_inner][/vc_row_inner][vc_row_inner el_class="margin-top-35"][vc_column_inner width="1/2"][vc_single_image image="2914" el_class="left rtl-right margin-right-20 rtl-margin-left-20 rtl-margin-right-0 margin-top-5"][nasa_title title_text="We work in Global" title_type="h5" title_desc="Lorem ipsum" el_class="nasa-clear-none"][/vc_column_inner][vc_column_inner width="1/2"][vc_single_image image="2915" el_class="left rtl-right margin-right-20 rtl-margin-left-20 rtl-margin-right-0 margin-top-5"][nasa_title title_text="Our guarantee" title_type="h5" title_desc="From 2 - 5 years" el_class="nasa-clear-none"][/vc_column_inner][/vc_row_inner][vc_row_inner el_class="margin-top-35"][vc_column_inner width="1/2"][vc_single_image image="2916" el_class="left rtl-right margin-right-20 rtl-margin-left-20 rtl-margin-right-0 margin-top-10"][nasa_title title_text="On the market" title_type="h5" title_desc="12 years" el_class="nasa-clear-none"][/vc_column_inner][vc_column_inner width="1/2"][vc_single_image image="2917" el_class="left rtl-right margin-right-20 rtl-margin-left-20 rtl-margin-right-0"][nasa_title title_text="Best quality" title_type="h5" title_desc="Lorem ipsum" el_class="nasa-clear-none"][/vc_column_inner][/vc_row_inner][/vc_column][/vc_row][vc_row el_class="margin-top-80 tablet-margin-top-60 mobile-margin-top-50 margin-bottom-30"][vc_column][nasa_title title_text="Latest Blog" title_type="h3" font_size="xl" title_desc="The freshest and most exciting news" title_align="text-center" el_class="margin-bottom-30"][/vc_column][vc_column][nasa_post dots="true" arrows="0" columns_number="3" columns_number_small="1" columns_number_tablet="2" readmore="no" page_blogs="no"][/vc_column][/vc_row][vc_row el_class="margin-top-50"][vc_column width="1/3"][nasa_products title_shortcode="Top Rated" type="top_rate" style="list_carousel" style_row="3" arrows="1" number="6" columns_number="1" columns_number_small="1" columns_number_tablet="2"][/vc_column][vc_column width="1/3"][nasa_products title_shortcode="Best Selling" type="best_selling" style="list_carousel" style_row="3" arrows="1" number="6" columns_number="1" columns_number_small="1" columns_number_tablet="2"][/vc_column][vc_column width="1/3"][nasa_products title_shortcode="On Sale" type="on_sale" style="list_carousel" style_row="3" arrows="1" number="6" columns_number="1" columns_number_small="1" columns_number_tablet="2"][/vc_column][/vc_row][vc_row el_class="margin-top-30 margin-bottom-50"][vc_column][nasa_brands images="' . elessi_imp_brands_str() . '" columns_number="6" columns_number_tablet="4" columns_number_small="3" custom_links="#,#,#,#,#,#,#"][/vc_column][/vc_row]'
+        ),
+        
+        'post_meta' => array(
+            // '_nasa_pri_color_flag' => 'on',
+            // '_nasa_pri_color' => '#87b66e',
+            '_wpb_shortcodes_custom_css' => '.vc_custom_1550418776704{background-color: #fed700 !important;background-position: 0 0 !important;background-repeat: repeat !important;}.vc_custom_1550403944446{background-color: #eeee22 !important;}'
+        ),
+        
+        'globals' => array(
+            'header-type' => '1',
+            
+            // 'fixed_nav' => '0',
+            
+            // 'plus_wide_width' => '200',
+            'color_primary' => '#87b66e',
+            
+            // 'fullwidth_main_menu' => '1',
+            
+            // 'bg_color_main_menu' => '#e4272c',
+            // 'text_color_main_menu' => '#ffffff',
+            
+            // 'bg_color_v_menu' => '#000000',
+            // 'text_color_v_menu' => '#ffffff',
+            
+            'footer_mode' => 'builder',
+            'footer-type' => 'footer-light-2',
+            'footer-mobile' => 'footer-mobile',
+            
+            // 'category_sidebar' => 'left',
+            
+            'product_detail_layout' => 'modern-3',
+            // 'product_image_layout' => 'single',
+            // 'product_image_style' => 'slide',
+            'sp_bgl' => '#f6f6f6',
+            // 'tab_style_info' => '2d-no-border',
+            
+            'loop_layout_buttons' => 'modern-2',
+            
+            // 'animated_products' => 'hover-carousel',
+            
+            // 'nasa_attr_image_style' => 'square',
+            // 'nasa_attr_image_single_style' => 'extends',
+            // 'nasa_attr_color_style' => 'round',
+            // 'nasa_attr_label_style' => 'round',
+            
+            'breadcrumb_row' => 'single',
+            'breadcrumb_type' => 'default',
+            'breadcrumb_bg_color' => '#f6f6f6',
+            'breadcrumb_align' => 'text-left',
+            'breadcrumb_height' => '60',
+        ),
+    );
+}
