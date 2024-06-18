a72e2f3bc39956c0578fd833f100c98211ca0922
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-06-18 12:45:42 +0500

    20

---
+++ themeforest-N37yxpwq-elessi-woocommerce-ajax-wordpress-theme 2/elessi-theme/admin/importer/data-import/pages-wpb/nasa_glasses-1.php
@@ -0,0 +1,155 @@
+<?php
+function nasa_wpb_glasses_1() {
+    $imgs_1 = elessi_import_upload('/wp-content/uploads/2023/10/glasses-slider1.jpg', '3105', array(
+        'post_title' => 'glasses-v1-slider1',
+        'post_name' => 'glasses-v1-slider1',
+    ));
+    
+    $imgs_2 = elessi_import_upload('/wp-content/uploads/2023/10/glasses-slider3.jpg', '3105', array(
+        'post_title' => 'glasses-v1-slider3',
+        'post_name' => 'glasses-v1-slider3',
+    ));
+    
+    $imgs_3 = elessi_import_upload('/wp-content/uploads/2023/10/glasses-banner2.jpg', '3106', array(
+        'post_title' => 'glasses-v1-banner2',
+        'post_name' => 'glasses-v1-banner2',
+    ));
+    
+    $imgs_4 = elessi_import_upload('/wp-content/uploads/2023/10/glasses-banner3.jpg', '3318', array(
+        'post_title' => 'glasses-v1-banner3',
+        'post_name' => 'glasses-v1-banner3',
+    ));
+    
+    $imgs_5 = elessi_import_upload('/wp-content/uploads/2023/10/glasses-banner4.jpg', '3318', array(
+        'post_title' => 'glasses-v1-banner4',
+        'post_name' => 'glasses-v1-banner4',
+    ));
+    
+    $imgs_6 = elessi_import_upload('/wp-content/uploads/2023/10/glasses-banner5.jpg', '3139', array(
+        'post_title' => 'glasses-v1-banner5',
+        'post_name' => 'glasses-v1-banner5',
+    ));
+    
+    $imgs_7 = elessi_import_upload('/wp-content/uploads/2023/10/glasses-banner6.jpg', '3139', array(
+        'post_title' => 'glasses-v1-banner6',
+        'post_name' => 'glasses-v1-banner6',
+    ));
+    
+    $imgs_8 = elessi_import_upload('/wp-content/uploads/2023/10/glasses-banner7.jpg', '3139', array(
+        'post_title' => 'glasses-v1-banner7',
+        'post_name' => 'glasses-v1-banner7',
+    ));
+    
+    $imgs_9 = elessi_import_upload('/wp-content/uploads/2023/10/glasses-return-icon.png', '2917', array(
+        'post_title' => 'glasses-v1-return-icon',
+        'post_name' => 'glasses-v1-return-icon',
+    ));
+    
+    $imgs_10 = elessi_import_upload('/wp-content/uploads/2023/10/glasses-support.png', '2916', array(
+        'post_title' => 'glasses-v1-support',
+        'post_name' => 'glasses-v1-support',
+    ));
+    
+    $imgs_11 = elessi_import_upload('/wp-content/uploads/2023/10/glasses-free-shipping.png', '2915', array(
+        'post_title' => 'glasses-v1-free-shipping',
+        'post_name' => 'glasses-v1-free-shipping',
+    ));
+    
+    $imgs_12 = elessi_import_upload('/wp-content/uploads/2023/10/glasses_delivery.png', '2914', array(
+        'post_title' => 'glasses-v1-delivery',
+        'post_name' => 'glasses-v1-delivery',
+    ));
+    
+    $imgs_13 = elessi_import_upload('/wp-content/uploads/2023/10/glasses-secure-purchase.png', '2917', array(
+        'post_title' => 'glasses-v1-secure-purchase',
+        'post_name' => 'glasses-v1-secure-purchase',
+    ));
+    
+    $imgs_14 = elessi_import_upload('/wp-content/uploads/2023/10/glasses-guarantee.png', '2916', array(
+        'post_title' => 'glasses-v1-guarantee',
+        'post_name' => 'glasses-v1-guarantee',
+    ));
+    
+    return array(
+        'post' => array(
+            'post_title' => 'WPB Glasses V1',
+            'post_name' => 'wpb-glasses-v1',
+            'post_content' => '[vc_row][vc_column width="1/2"][vc_row_inner][vc_column_inner][nasa_slider bullets_pos="inside" bullets_align="left" column_number="1" column_number_small="1" column_number_tablet="1" el_class="nasa-over-hide force-radius-10 bulet-margin-l55"][nasa_banner_2 img_src="' . $imgs_1 . '" el_class="nasa-bold-500"]
+<div class="fs-14 hide-for-small hide-for-medium" style="color: #979797; line-height: 1.4;">PURE AND NATURE</div>
+<div class="fs-35 tablet-fs-25 mobile-fs-30 margin-top-10 margin-bottom-10" style="line-height: 1.2; color: #333333;">Summer
+Collection 2023</div>
+<div class="fs-14 hide-for-small" style="color: #979797; line-height: 1.4;">Today discount
+<span class="fs-40 hide-for-small primary-color nasa-bold-500" style="position: relative;">30<span style="position: absolute; top: 5px; left: 100%; font-size: 15px;">%</span></span></div>
+<a class="button ns-baner_btn_zoom small fs-16 force-radius-20" style="text-transform: capitalize; letter-spacing: 0; background: transparent; color: #000; border: 2px #000 solid; margin-top: 10px; padding: 15px; height: 40px;" tabindex="0" title="Shop now" href="#">Shop Now<span class="fs-20 margin-left-5">→</span></a>[/nasa_banner_2][nasa_banner_2 align="right" valign="middle" text_align="text-center" img_src="' . $imgs_2 . '" el_class="nasa-bold-500"]
+<div class="fs-14 hide-for-small hide-for-medium margin-top-25" style="color: #979797; line-height: 1.4;">PURE AND NATURE</div>
+<div class="fs-35 tablet-fs-25 mobile-fs-20 margin-top-10" style="line-height: 1.2; color: #333333;">Summer
+Collection 2023</div>
+<a class="margin-top-20 ns-baner_btn_zoom button small fs-16 force-radius-20" style="height: 40px; text-transform: capitalize; letter-spacing: 0; background: transparent; color: #000; border: 2px #000 solid;" title="Shop now" href="#">Shop Now</a>[/nasa_banner_2][/nasa_slider][/vc_column_inner][/vc_row_inner][/vc_column][vc_column width="1/2"][vc_row_inner][vc_column_inner el_class="mobile-padding-top-10 mobile-padding-bottom-10" width="1/2"][nasa_banner_2 valign="bottom" img_src="' . $imgs_3 . '" el_class="nasa-over-hide force-radius-10 nasa-bold-500 mobile-margin-bottom-0"]
+<div class="fs-20" style="color: #000; line-height: 1.4;">Sale up to
+<span class="fs-30 primary-color hide-for-medium " style="position: relative;">30% OFF
+</span></div>
+<a class="button ns-baner_btn_zoom small fs-16 force-radius-20" style="text-transform: capitalize; height: 40px; letter-spacing: 0; background: transparent; color: #000; border: 2px #000 solid; margin-top: 10px; padding: 0 15px;" title="Shop now" href="#">Shop Now<span class="fs-20 margin-left-5">→</span></a>[/nasa_banner_2][/vc_column_inner][vc_column_inner width="1/2"][nasa_banner_2 img_src="' . $imgs_4 . '" el_class="nasa-over-hide force-radius-10 nasa-bold-500"]<span class="fs-30 hide-for-medium " style="line-height: 1.2; position: relative;">Sport
+Collection</span>
+<a class="button ns-baner_btn_zoom small fs-16 force-radius-20" style="text-transform: capitalize; letter-spacing: 0; background: transparent; color: #000; border: none; margin-top: 5px; padding: 0;" title="Shop now" href="#">Shop Now <span class="fs-20 margin-left-5">→</span></a>[/nasa_banner_2][nasa_banner_2 img_src="' . $imgs_5 . '" el_class="nasa-over-hide force-radius-10"][/nasa_banner_2][/vc_column_inner][/vc_row_inner][/vc_column][/vc_row][vc_row el_class="margin-top-35 mobile-margin-top-30"][vc_column][nasa_title title_text="Shop by Categories" title_desc="Discover your perfect pair with our curated categories" title_align="text-center" font_size="l" el_class="margin-bottom-15"][nasa_product_categories list_cats="grooming-men, shoes-men, clothing-woman, jewelry-woman, beauty, kid, accessories-men" disp_type="Horizontal6" columns_number="7" columns_number_small="2" columns_number_tablet="4" el_class="items-r50 items-padding-20px"][/vc_column][/vc_row][vc_row el_class="margin-top-30 mobile-margin-top-10"][vc_column][nasa_title title_text="Featured Products" title_align="text-center" font_size="l" el_class="margin-bottom-10"][vc_tta_tabs alignment="center" el_class="ns-no-underline tab-right-60 letter-spacing-none"][vc_tta_section section_nasa_img="21917" title="Geometrical" tab_id="1697535636023-ef031553-1390"][nasa_products style="carousel" arrows="0" number="6" columns_number="5" columns_number_tablet="3"][/vc_tta_section][vc_tta_section section_nasa_img="21918" title="Pilot" tab_id="1697535636035-310b0c5c-80ee"][nasa_products type="best_selling" style="carousel" arrows="0" number="6" columns_number="5" columns_number_tablet="3"][/vc_tta_section][vc_tta_section section_nasa_img="21919" title="Rectagle" tab_id="1697537529667-5392dbaf-0a24"][nasa_products type="featured_product" style="carousel" arrows="0" number="6" columns_number="5" columns_number_tablet="3"][/vc_tta_section][vc_tta_section section_nasa_img="21920" title="Round" tab_id="1697538014342-c7d4b257-f32a"][nasa_products style="carousel" arrows="0" number="66" columns_number="5" columns_number_tablet="3"][/vc_tta_section][vc_tta_section section_nasa_img="21921" title="Square" tab_id="1697538051002-6fd37e04-6418"][nasa_products type="best_selling" style="carousel" arrows="0" number="6" columns_number="5" columns_number_tablet="3"][/vc_tta_section][/vc_tta_tabs][vc_column_text css=".vc_custom_1698918209180{margin-top: 10px !important;}"]
+<p style="text-align: center;"><a class="button small ns-baner_btn_zoom fs-16" style="padding-left: 50px; height: 50px; border-radius: 100px; padding-right: 50px; text-transform: none; letter-spacing: 0; background: transparent; color: #000; border: 2px #000 solid;" title="Shop best seller" href="#">Shop best seller <span class="fs-20 margin-left-5">→</span></a></p>
+[/vc_column_text][/vc_column][/vc_row][vc_row el_class="margin-top-70 mobile-margin-top-50"][vc_column width="1/3"][nasa_banner_2 valign="middle" hover="zoom" img_src="' . $imgs_6 . '" el_class="nasa-bold-500"]
+<div class="fs-25 tablet-fs-20 mobile-fs-20 margin-top-10 margin-bottom-10" style="line-height: 1.2; color: #333333;">Summer
+Collection</div>
+<div class="fs-20 tablet-fs-20 mobile-fs-20 nasa-flex jc margin-top-20" style="line-height: 1.2; color: #333333;">Sale off<span class="fs-20 primary-color" style="position: relative; margin-left: 5px;">25%</span></div>
+[/nasa_banner_2][/vc_column][vc_column width="1/3"][nasa_banner_2 valign="middle" hover="zoom" img_src="' . $imgs_7 . '" el_class="nasa-bold-500"]
+<div class="fs-14" style="color: #979797; line-height: 1.4;">BEST CHOICE</div>
+<div class="fs-25 tablet-fs-20 mobile-fs-20 margin-top-20 margin-bottom-10" style="line-height: 1.2; color: #333333;">Glasses
+&amp; Sunglasses</div>
+<div class="fs-18 tablet-fs-20 mobile-fs-20 margin-top-20" style="line-height: 1.4; color: #333333;">FROM
+<span class="fs-20 hide-for-medium primary-color" style="position: relative;">$99.00</span></div>
+[/nasa_banner_2][/vc_column][vc_column width="1/3"][nasa_banner_2 valign="middle" hover="zoom" img_src="' . $imgs_8 . '" el_class="nasa-bold-500"]
+<div class="fs-25 tablet-fs-20 mobile-fs-20 margin-bottom-20" style="line-height: 1.2; color: #333333;">Summer
+Collection</div>
+<a class="button ns-baner_btn_zoom small fs-16 force-radius-20" style="text-transform: capitalize; height: 40px; letter-spacing: 0; background: transparent; color: #000; border: 2px #000 solid; margin-top: 10px; padding: 0 15px;" title="Shop now" href="#">Shop Now<span class="fs-20 margin-left-5">→</span> </a>[/nasa_banner_2][/vc_column][/vc_row][vc_row el_class="margin-top-35"][vc_column][nasa_title title_text="Summer Collection" title_desc="Collect your loves with our summer arrivals" title_align="text-center" font_size="l" el_class="margin-bottom-30"][nasa_products type="featured_product" style="carousel" arrows="0" number="6" columns_number="5" columns_number_tablet="3"][/vc_column][/vc_row][vc_row css=".vc_custom_1699086568892{background-position: 0 0 !important;background-repeat: repeat !important;}" el_class="margin-bottom-80 mobile-margin-bottom-50 padding-top-15 padding-bottom-15 primary-bg"][vc_column][nasa_slider bullets="false" navigation="false" autoplay="true"][vc_column_text]
+<p class="text-center" style="color: #fff;"><strong>UP TO 70% OFF THE ENTRIRE STORE! - MADE WITH LOVE by <span class="nasa-underline">Nasa studio</span></strong></p>
+[/vc_column_text][vc_column_text]
+<p class="text-center" style="color: #fff;"><strong>UP TO 70% OFF THE ENTRIRE STORE! - MADE WITH LOVE by <span class="nasa-underline">Nasa studio</span></strong></p>
+[/vc_column_text][/nasa_slider][/vc_column][/vc_row][vc_row el_class="margin-top-70 mobile-margin-top-50"][vc_column width="1/2"][vc_video link="https://www.youtube.com/watch?v=P37obqUUVYE"][/vc_column][vc_column width="1/2" el_class="desktop-padding-left-40 desktop-padding-right-40 mobile-padding-left-10 mobile-padding-right-10"][vc_row_inner el_class="mobile-margin-top-30"][vc_column_inner][nasa_title title_text="We promise" font_size="xl" el_class="fs-39"][/vc_column_inner][/vc_row_inner][vc_column_text]Our mission is to bring together a diverse, curated collection of beautiful furniture and homewares from around the world. I was popularised in the 1960s with the release.[/vc_column_text][vc_row_inner el_class="margin-top-20"][vc_column_inner width="1/2" css=".vc_custom_1698307001509{padding-top: 10px !important;padding-right: 10px !important;padding-bottom: 10px !important;padding-left: 10px !important;}"][vc_single_image image="' . $imgs_9 . '" el_class="left rtl-right margin-right-20 rtl-margin-left-20 rtl-margin-right-0 margin-top-5"][nasa_title title_text="Fast Delivery" title_type="h5" title_desc="Get your order fast" el_class="nasa-clear-none mobile-margin-bottom-0"][/vc_column_inner][vc_column_inner width="1/2" css=".vc_custom_1698307424175{padding-top: 10px !important;padding-right: 10px !important;padding-bottom: 10px !important;padding-left: 10px !important;}"][vc_single_image image="' . $imgs_10 . '" el_class="left rtl-right margin-right-20 rtl-margin-left-20 rtl-margin-right-0 margin-top-5"][nasa_title title_text="Customer Support" title_type="h5" title_desc="Our priority" el_class="nasa-clear-none mobile-margin-bottom-0"][/vc_column_inner][/vc_row_inner][vc_row_inner el_class="desktop-margin-top-10 desktop-margin-bottom-10"][vc_column_inner width="1/2" css=".vc_custom_1698307156276{padding-top: 10px !important;padding-right: 10px !important;padding-bottom: 10px !important;padding-left: 10px !important;}"][vc_single_image image="' . $imgs_11 . '" el_class="left rtl-right margin-right-20 rtl-margin-left-20 rtl-margin-right-0 margin-top-5"][nasa_title title_text="Free Shipping" title_type="h5" title_desc="Order over 100$" el_class="nasa-clear-none mobile-margin-bottom-0"][/vc_column_inner][vc_column_inner width="1/2" css=".vc_custom_1698307428812{padding-top: 10px !important;padding-right: 10px !important;padding-bottom: 10px !important;padding-left: 10px !important;}"][vc_single_image image="' . $imgs_12 . '" el_class="left rtl-right margin-right-20 rtl-margin-left-20 rtl-margin-right-0 margin-top-5"][nasa_title title_text="30 Days Returns" title_type="h5" title_desc="No question ask" el_class="nasa-clear-none mobile-margin-bottom-0"][/vc_column_inner][/vc_row_inner][vc_row_inner][vc_column_inner width="1/2" css=".vc_custom_1698307172029{padding-top: 10px !important;padding-right: 10px !important;padding-bottom: 10px !important;padding-left: 10px !important;}"][vc_single_image image="' . $imgs_13 . '" el_class="left rtl-right margin-right-20 rtl-margin-left-20 rtl-margin-right-0 margin-top-5"][nasa_title title_text="Secure Payment" title_type="h5" title_desc="Lorem ip sum" el_class="nasa-clear-none mobile-margin-bottom-0"][/vc_column_inner][vc_column_inner width="1/2" css=".vc_custom_1698307433085{padding-top: 10px !important;padding-right: 10px !important;padding-bottom: 10px !important;padding-left: 10px !important;}"][vc_single_image image="' . $imgs_14 . '" el_class="left rtl-right margin-right-20 rtl-margin-left-20 rtl-margin-right-0 margin-top-5"][nasa_title title_text="Lifetime Warranty" title_type="h5" title_desc="For original onwers" el_class="nasa-clear-none mobile-margin-bottom-0"][/vc_column_inner][/vc_row_inner][/vc_column][/vc_row][vc_row el_class="margin-top-50 mobile-margin-top-25 desktop-margin-bottom-30"][vc_column el_class="margin-top-5"][nasa_title title_text="Latest Blog" title_desc="The freshest and most exiting news" title_align="text-center" font_size="l" el_class="margin-bottom-30"][nasa_post arrows="1" columns_number="4" columns_number_small_slider="1" columns_number_tablet="2" readmore="no" page_blogs="no"][/vc_column][/vc_row]'
+        ),
+        
+        'post_meta' => array(
+            // '_nasa_header_block' => 'static-header-1',
+            // '_nasa_el_class_header' => 'main-home-fix',
+            '_wpb_shortcodes_custom_css' => '.vc_custom_1698918209180{margin-top: 10px !important;}.vc_custom_1698307001509{padding-top: 10px !important;padding-right: 10px !important;padding-bottom: 10px !important;padding-left: 10px !important;}.vc_custom_1698307424175{padding-top: 10px !important;padding-right: 10px !important;padding-bottom: 10px !important;padding-left: 10px !important;}.vc_custom_1698307156276{padding-top: 10px !important;padding-right: 10px !important;padding-bottom: 10px !important;padding-left: 10px !important;}.vc_custom_1698307428812{padding-top: 10px !important;padding-right: 10px !important;padding-bottom: 10px !important;padding-left: 10px !important;}.vc_custom_1698307172029{padding-top: 10px !important;padding-right: 10px !important;padding-bottom: 10px !important;padding-left: 10px !important;}.vc_custom_1698307433085{padding-top: 10px !important;padding-right: 10px !important;padding-bottom: 10px !important;padding-left: 10px !important;}'
+        ),
+        
+        'globals' => array(
+            'header-type' => '1',
+            
+            'footer_mode' => 'builder',
+            'footer-type' => 'footer-light-2',
+            'footer-mobile' => 'footer-mobile',
+            
+            'plus_wide_width' => '400',
+            'color_primary' => '#d89701',
+            
+            // 'category_sidebar' => 'left',
+            
+            'product_detail_layout' => 'modern-2',
+            'sp_bgl' => '#f4f4f4',
+            // 'product_image_layout' => 'single',
+            // 'product_image_style' => 'scroll',
+            // 'tab_style_info' => 'ver-2',
+            
+            'loop_layout_buttons' => 'modern-5',
+            
+            // 'animated_products' => 'hover-carousel',
+            
+            // 'nasa_attr_image_style' => 'square',
+            // 'nasa_attr_image_single_style' => 'extends',
+            // 'nasa_attr_color_style' => 'round',
+            // 'nasa_attr_label_style' => 'round',
+            
+            'breadcrumb_row' => 'single',
+            'breadcrumb_type' => 'default',
+            'breadcrumb_bg_color' => '#f4f4f4',
+            'breadcrumb_align' => 'text-left',
+            'breadcrumb_height' => '60'
+        ),
+    );
+}
