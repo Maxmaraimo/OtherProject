be27c9ef65cdd6169cebac8c9cacc00cd70619a0
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-06-18 12:43:53 +0500

    2

---
+++ themeforest-N37yxpwq-elessi-woocommerce-ajax-wordpress-theme 2/elessi-theme/admin/importer/data-import/pages-wpb/nasa_auto-parts-3.php
@@ -0,0 +1,114 @@
+<?php
+function nasa_wpb_auto_parts_3() {
+    $imgs_1 = elessi_import_upload('/wp-content/uploads/2021/10/h6-slider3.jpeg', '3142', array(
+        'post_title' => 'h6-slider3',
+        'post_name' => 'h6-slider3',
+    ));
+    
+    $imgs_2 = elessi_import_upload('/wp-content/uploads/2021/10/h6-slider-4.jpeg', '3142', array(
+        'post_title' => 'h6-slider-4',
+        'post_name' => 'h6-slider-4',
+    ));
+    
+    $imgs_3 = elessi_import_upload('/wp-content/uploads/2021/10/h6-banner1.jpg', '3117', array(
+        'post_title' => 'h6-banner1',
+        'post_name' => 'h6-banner1',
+    ));
+    
+    $imgs_4 = elessi_import_upload('/wp-content/uploads/2021/10/h6-banner2.jpg', '3117', array(
+        'post_title' => 'h6-banner2',
+        'post_name' => 'h6-banner2',
+    ));
+    
+    $imgs_5 = elessi_import_upload('/wp-content/uploads/2021/10/h6-deal-bg-3.jpeg', '3152', array(
+        'post_title' => 'h6-deal-bg-3',
+        'post_name' => 'h6-deal-bg-3',
+    ));
+    
+    return array(
+        'post' => array(
+            'post_title' => 'WPB Auto Parts V3',
+            'post_name' => 'wpb-auto-parts-v3',
+            'post_content' => '[vc_row equal_height="yes" el_class="margin-bottom-50 margin-top-20 mobile-margin-top-10"][vc_column width="1/4" el_class="mobile-margin-bottom-10"][vc_row_inner equal_height="yes" el_class="margin-left-0 margin-right-0" css=".vc_custom_1634116475379{background-color: #f6f6f6 !important;}"][vc_column_inner el_class="jc padding-top-70 mobile-padding-top-35 padding-bottom-50 desktop-padding-left-20 desktop-padding-right-20"][vc_column_text el_class="margin-bottom-50 nasa-fullwidth"]
+<h2 class="text-center" style="color: #000;">Select Your Vehicle</h2>
+<h6 class="text-center margin-top-15" style="letter-spacing: 3px; line-height: 1.4;">OVER 100.000
+AUTO AND TRUCK PARTS</h6>
+[/vc_column_text][nasa_product_nasa_categories style="ver" count_items="1" button_text="Find My Part"][/vc_column_inner][/vc_row_inner][/vc_column][vc_column width="3/4" el_class="rtl-left"][nasa_slider bullets_pos="inside" navigation="false" column_number="1" column_number_small="1" column_number_tablet="1" autoplay="true" el_class="right-now"][nasa_banner move_x="5%" banner_responsive="no" effect_text="fadeInLeft" img_src="' . $imgs_1 . '"]
+<h6 style="color: #999;">LIMITED EDITION</h6>
+<h2 style="font-size: 300%;">High end
+Audio Electronics</h2>
+<h5 style="margin-top: 20px; color: #999;">Sale up to<span style="color: #539309; font-size: 200%;"> 30% Off</span></h5>
+<a class="button margin-top-30" style="font-size: 12px; border-radius: 5px;" title="Shop now" href="#">Shop Now</a>[/nasa_banner][nasa_banner move_x="5%" banner_responsive="no" effect_text="fadeInLeft" img_src="' . $imgs_2 . '"]
+<h6 style="color: #999;">SPECICAL OFFER</h6>
+<h2 style="font-size: 300%;">Custom Rims
+Best Perfomance</h2>
+<h5 style="margin-top: 20px; color: #999;">Sale up to<span style="color: #539309; font-size: 200%;"> 20% Off</span></h5>
+<a class="button margin-top-30" style="font-size: 12px; border-radius: 5px;" title="Shop now" href="#">Shop Now</a>[/nasa_banner][/nasa_slider][/vc_column][/vc_row][vc_row el_class="margin-bottom-40 mobile-margin-bottom-0"][vc_column width="1/4" offset="vc_col-xs-12" el_class="mobile-margin-bottom-30"][nasa_service_box service_hover="buzz_effect" service_title="Free Delivery" service_desc="Free Shipping for all Order" service_icon="icon-nasa-car-2"][/vc_column][vc_column width="1/4" offset="vc_col-xs-12" el_class="mobile-margin-bottom-30"][nasa_service_box service_hover="buzz_effect" service_title="Support 24/7" service_desc="We support 24h a day" service_icon="icon-nasa-headphone"][/vc_column][vc_column width="1/4" offset="vc_col-xs-12" el_class="mobile-margin-bottom-30"][nasa_service_box service_hover="buzz_effect" service_title="100% Money Back" service_desc="You have 30 days to Return" service_icon="icon-nasa-refresh"][/vc_column][vc_column width="1/4" offset="vc_col-xs-12" el_class="mobile-margin-bottom-30"][nasa_service_box service_hover="buzz_effect" service_title="Payment Secure" service_desc="We ensure secure payment" service_icon="icon-nasa-credit"][/vc_column][/vc_row][vc_row el_class="margin-bottom-30 mobile-margin-bottom-50"][vc_column width="1/2"][nasa_banner valign="middle" banner_responsive="no" hover="zoom" img_src="' . $imgs_3 . '"]
+<h4 style="font-weight: 300 !important;">Moteri racing</h4>
+<h4 style="font-weight: 300 !important;">Rally <b>Gold Custom</b></h4>
+<h6 style="margin-top: 20px;">Just Only   <span style="color: #539309; font-size: 180%;">$120.00</span></h6>
+<a class="button margin-top-30" style="border-radius: 5px; letter-spacing: 1.5px;" title="Shop now" href="#">Shop Now</a>[/nasa_banner][/vc_column][vc_column width="1/2"][nasa_banner valign="middle" banner_responsive="no" hover="zoom" img_src="' . $imgs_4 . '"]
+<h4>Auto Repair</h4>
+<h4>System <span style="font-weight: 300;">Accessories</span></h4>
+<h6 style="margin-top: 20px;">Discount   <span style="color: #ff0000; font-size: 180%;">25% Off</span></h6>
+<a class="button margin-top-30" style="border-radius: 5px; letter-spacing: 1.5px;" title="Shop now" href="#">Shop Now</a>[/nasa_banner][/vc_column][/vc_row][vc_row el_class="margin-bottom-40 mobile-margin-bottom-30"][vc_column][vc_tta_tabs alignment="right" tabs_display_type="2d-has-bg" tabs_bg_color="" title="What we offer" el_class="letter-spacing-none"][vc_tta_section title="New Arrivals" tab_id="1561625360051-6c065a30-3c9a"][nasa_products number="10" columns_number="5" columns_number_tablet="3"][/vc_tta_section][vc_tta_section title="Featured" tab_id="1561625360195-a16b5ca8-e63c"][nasa_products type="featured_product" number="10" columns_number="5" columns_number_tablet="3"][/vc_tta_section][vc_tta_section title="Popular" tab_id="1561625410512-6ee31f0d-68e5"][nasa_products type="best_selling" number="10" columns_number="5" columns_number_tablet="3"][/vc_tta_section][vc_tta_section title="On Sale" tab_id="1561625412628-7fb4c6bc-0ce9"][nasa_products type="on_sale" number="10" columns_number="5" columns_number_tablet="3"][/vc_tta_section][/vc_tta_tabs][/vc_column][/vc_row][vc_row parallax="content-moving" parallax_image="' . $imgs_5 . '" el_class="margin-bottom-80 mobile-margin-bottom-30"][vc_column width="7/12" el_class="medium-12 desktop-padding-left-80 tablet-padding-left-40 tablet-padding-right-40 padding-top-50 mobile-padding-top-20 desktop-padding-bottom-50 mobile-padding-bottom-10"][nasa_products_special_deal limit="3" style="for_time" date_sc="2024-12-30" columns_number="2" columns_number_small="2" columns_number_tablet="3" arrows="1" auto_slide="false" category_product="wheels-tires" title="Deal of the day" desc_shortcode="Sale 35% for all T-Shirt products"][/vc_column][vc_column width="5/12"][/vc_column][/vc_row][vc_row el_class="margin-bottom-40 mobile-margin-bottom-0"][vc_column width="1/3" el_class="mobile-margin-top-20"][nasa_products title_shortcode="Top Rated" type="top_rate" style="list_carousel" style_row="3" arrows="1" number="6" columns_number="1" columns_number_tablet="1" columns_number_small="1"][/vc_column][vc_column width="1/3" el_class="mobile-margin-top-30"][nasa_products title_shortcode="Best Selling" type="best_selling" style="list_carousel" style_row="3" arrows="1" number="6" columns_number="1" columns_number_tablet="1" columns_number_small="1"][/vc_column][vc_column width="1/3" el_class="mobile-margin-top-30"][nasa_products title_shortcode="On Sale" type="on_sale" style="list_carousel" style_row="3" arrows="1" number="6" columns_number="1" columns_number_tablet="1" columns_number_small="1"][/vc_column][/vc_row][vc_row el_class="margin-bottom-30 mobile-margin-bottom-10"][vc_column][nasa_brands images="' . elessi_imp_brands_str() . '" columns_number="6" columns_number_tablet="4" columns_number_small="3" custom_links="#,#,#,#,#,#,#"][/vc_column][/vc_row]'
+        ),
+        'post_meta' => array(
+            // '_nasa_custom_header' => '4',
+            // '_nasa_plus_wide_option' => '1',
+            // '_nasa_plus_wide_width' => '100',
+            // '_nasa_pri_color_flag' => 'on',
+            // '_nasa_pri_color' => '#dd3333',
+            // '_nasa_bg_color_main_menu' => '#e4272c',
+            // '_nasa_text_color_main_menu' => '#ffffff',
+            // '_nasa_bg_color_v_menu' => '#000000',
+            // '_nasa_text_color_v_menu' => '#ffffff',
+            // '_nasa_type_font_select' => 'google',
+            // '_nasa_type_headings' => 'Rubik',
+            // '_nasa_type_texts' => 'Rubik',
+            // '_nasa_type_nav' => 'Rubik',
+            // '_nasa_type_banner' => 'Rubik',
+            // '_nasa_type_price' => 'Rubik',
+            // '_nasa_font_weight' => '500',
+            '_wpb_shortcodes_custom_css' => '.vc_custom_1634116475379{background-color: #f6f6f6 !important;}'
+        ),
+        
+        'globals' => array(
+            'header-type' => '4',
+            
+            'fixed_nav' => '0',
+            
+            'plus_wide_width' => '100',
+            'color_primary' => '#dd3333',
+            
+            // 'fullwidth_main_menu' => '1',
+            
+            'bg_color_main_menu' => '#e4272c',
+            'text_color_main_menu' => '#ffffff',
+            
+            'bg_color_v_menu' => '#000000',
+            'text_color_v_menu' => '#ffffff',
+            
+            'footer_mode' => 'builder',
+            'footer-type' => 'footer-light-2',
+            'footer-mobile' => 'footer-mobile',
+            
+            'category_sidebar' => 'top-2',
+            
+            'nasa_attr_label_style' => 'small-square-2',
+            
+            'product_detail_layout' => 'modern-1',
+            // 'tab_style_info' => 'scroll-down',
+            
+            'loop_layout_buttons' => 'modern-3',
+            
+            // 'animated_products' => 'hover-fade',
+            
+            // 'nasa_attr_image_style' => 'square'
+            
+            'breadcrumb_row' => 'single',
+            'breadcrumb_align' => 'text-left',
+            'breadcrumb_height' => '60',
+        ),
+    );
+}
