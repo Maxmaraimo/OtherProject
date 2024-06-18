f53773f827e9d3bc3f99b666ef5eada07a57b43f
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-06-18 12:45:31 +0500

    17

---
+++ themeforest-N37yxpwq-elessi-woocommerce-ajax-wordpress-theme 2/elessi-theme/admin/importer/data-import/pages-wpb/nasa_fashion-7.php
@@ -0,0 +1,51 @@
+<?php
+function nasa_wpb_fashion_7() {
+    return array(
+        'post' => array(
+            'post_title' => 'WPB Fashion V7',
+            'post_name' => 'wpb-fashion-v7',
+            'post_content' => '[vc_row el_class="desktop-margin-top-30"][vc_column width="1/2"][vc_row_inner][vc_column_inner el_class="desktop-padding-right-5" width="1/2"][nasa_pin_products_banner pin_slug="pin-banner-285x260" bg_icon="#ffffff" txt_color="#333333"][/vc_column_inner][vc_column_inner el_class="desktop-padding-left-5 mobile-margin-top-10" width="1/2"][nasa_pin_products_banner pin_slug="pin-banner-285x260" bg_icon="#ffffff" txt_color="#333333"][/vc_column_inner][/vc_row_inner][vc_row_inner][vc_column_inner el_class="margin-top-10"][nasa_pin_products_banner pin_slug="pin-banner-580x530" bg_icon="#ffffff" txt_color="#333333"][/vc_column_inner][/vc_row_inner][vc_row_inner el_class="margin-bottom-10"][vc_column_inner el_class="desktop-padding-right-5 margin-top-10" width="1/2"][nasa_pin_products_banner pin_slug="pin-banner-285x260" bg_icon="#ffffff" txt_color="#333333"][/vc_column_inner][vc_column_inner el_class="desktop-padding-left-5 margin-top-10" width="1/2"][nasa_pin_products_banner pin_slug="pin-banner-285x260" bg_icon="#ffffff" txt_color="#333333"][/vc_column_inner][/vc_row_inner][/vc_column][vc_column width="1/2" el_class="desktop-padding-left-0"][vc_row_inner][vc_column_inner el_class="desktop-padding-right-5 margin-bottom-10" width="1/2"][nasa_pin_products_banner pin_slug="pin-banner-290x530" bg_icon="#ffffff" txt_color="#333333"][/vc_column_inner][vc_column_inner el_class="desktop-padding-left-5 margin-bottom-10" width="1/2"][nasa_pin_products_banner pin_slug="pin-banner-290x260" bg_icon="#ffffff" txt_color="#333333"][nasa_pin_products_banner pin_slug="pin-banner-290x260" bg_icon="#ffffff" txt_color="#333333" el_class="margin-top-10"][/vc_column_inner][/vc_row_inner][vc_row_inner][vc_column_inner][nasa_pin_products_banner pin_slug="pin-banner-590x260" bg_icon="#ffffff" txt_color="#333333"][/vc_column_inner][/vc_row_inner][vc_row_inner][vc_column_inner el_class="margin-top-10"][nasa_pin_products_banner pin_slug="pin-banner-590x260" bg_icon="#ffffff" txt_color="#333333"][/vc_column_inner][/vc_row_inner][/vc_column][/vc_row][vc_row][vc_column width="1/4" el_class="desktop-padding-right-5 mobile-margin-top-10"][nasa_pin_products_banner pin_slug="pin-banner-285x260" bg_icon="#ffffff" txt_color="#333333"][nasa_pin_products_banner pin_slug="pin-banner-285x260" bg_icon="#ffffff" txt_color="#333333" el_class="margin-top-10"][/vc_column][vc_column width="1/2" el_class="desktop-padding-left-5 mobile-margin-top-10"][nasa_pin_products_banner pin_slug="pin-banner-580x525" bg_icon="#ffffff" txt_color="#333333"][/vc_column][vc_column width="1/4" el_class="desktop-padding-left-0 mobile-margin-top-10"][nasa_pin_products_banner pin_slug="pin-banner-290x530" bg_icon="#ffffff" txt_color="#333333"][/vc_column][/vc_row][vc_row fullwidth="1" el_class="margin-top-80"][vc_column][nasa_title title_text="Follow us on Instagram" title_type="h4" title_align="text-center" el_class="desktop-margin-bottom-20"][nasa_instagram_feed username_show="Street Style" instagram_link="#"][/vc_column][/vc_row][vc_row css=".vc_custom_1524284991626{margin-bottom: 50px !important;padding-top: 30px !important;}"][vc_column][nasa_brands images="' . elessi_imp_brands_str() . '" columns_number="6" columns_number_tablet="4" columns_number_small="3" custom_links="#,#,#,#,#,#,#"][/vc_column][/vc_row]'
+        ),
+        'post_meta' => array(
+            // '_nasa_custom_header' => '7',
+            // '_nasa_bg_color_main_menu' => '#ffffff',
+            // '_nasa_text_color_main_menu' => '#333333',
+            // '_nasa_fullwidth_main_menu' => '1',
+            '_nasa_el_class_header' => 'menu-header-margin-top-0',
+            '_wpb_shortcodes_custom_css' => '.vc_custom_1524284991626{margin-bottom: 50px !important;padding-top: 30px !important;}'
+        ),
+        
+        'globals' => array(
+            'header-type' => '7',
+            
+            'fullwidth_main_menu' => '1',
+            
+            'bg_color_main_menu' => '#ffffff',
+            'text_color_main_menu' => '#333',
+            
+            'footer_mode' => 'builder',
+            'footer-type' => 'footer-light-2',
+            'footer-mobile' => 'footer-mobile',
+            
+            // 'category_sidebar' => 'left',
+            
+            // 'product_detail_layout' => 'modern-1',
+            // 'product_image_layout' => 'single',
+            // 'product_image_style' => 'slide',
+            'tab_style_info' => 'ver-2',
+            
+            // 'loop_layout_buttons' => 'hoz-buttons',
+            
+            // 'animated_products' => 'hover-carousel',
+            
+            // 'nasa_attr_image_style' => 'square',
+            // 'nasa_attr_image_single_style' => 'extends',
+            'nasa_attr_color_style' => 'round',
+            'nasa_attr_label_style' => 'round',
+            
+            // 'breadcrumb_row' => 'single',
+            // 'breadcrumb_type' => 'default',
+            // 'breadcrumb_bg_color' => '#f8f8f8',
+        ),
+    );
+}
