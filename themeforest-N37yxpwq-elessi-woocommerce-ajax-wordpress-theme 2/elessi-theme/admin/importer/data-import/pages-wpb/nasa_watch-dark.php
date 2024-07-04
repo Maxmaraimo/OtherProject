c3fb4b7b41f9ada1d93ba5752349b36c1fdec19c
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-07-04 16:28:23 +0500

    18

---
+++ themeforest-N37yxpwq-elessi-woocommerce-ajax-wordpress-theme 2/elessi-theme/admin/importer/data-import/pages-wpb/nasa_watch-dark.php
@@ -0,0 +1,151 @@
+<?php
+function nasa_wpb_watch_dark() {
+    $imgs_1 = elessi_import_upload('/wp-content/uploads/2016/06/watches_dark_banner3_bg.jpg', '3117', array(
+        'post_title' => 'watches_dark_banner3_bg',
+        'post_name' => 'watches_dark_banner3_bg',
+    ));
+    
+    $imgs_2 = elessi_import_upload('/wp-content/uploads/2016/06/watches_dark_banner4_bg.jpg', '3117', array(
+        'post_title' => 'watches_dark_banner4_bg',
+        'post_name' => 'watches_dark_banner4_bg',
+    ));
+    
+    $imgs_3 = elessi_import_upload('/wp-content/uploads/2021/02/watches_dark_collection_1.jpg', '3318', array(
+        'post_title' => 'watches_dark_collection_1',
+        'post_name' => 'watches_dark_collection_1',
+    ));
+    
+    $imgs_4 = elessi_import_upload('/wp-content/uploads/2021/02/watches_dark_collection_2.jpg', '3318', array(
+        'post_title' => 'watches_dark_collection_2',
+        'post_name' => 'watches_dark_collection_2',
+    ));
+    
+    $imgs_5 = elessi_import_upload('/wp-content/uploads/2021/02/watches_dark_collection_3.jpg', '3318', array(
+        'post_title' => 'watches_dark_collection_3',
+        'post_name' => 'watches_dark_collection_3',
+    ));
+    
+    $imgs_6 = elessi_import_upload('/wp-content/uploads/2021/02/watches_dark_collection_4.jpg', '3318', array(
+        'post_title' => 'watches_dark_collection_4',
+        'post_name' => 'watches_dark_collection_4',
+    ));
+    
+    $imgs_7 = elessi_import_upload('/wp-content/uploads/2021/02/watches_dark_collection_5.jpg', '3318', array(
+        'post_title' => 'watches_dark_collection_5',
+        'post_name' => 'watches_dark_collection_5',
+    ));
+    
+    $imgs_8 = elessi_import_upload('/wp-content/uploads/2021/02/watches_dark_collection_6.jpg', '3318', array(
+        'post_title' => 'watches_dark_collection_6',
+        'post_name' => 'watches_dark_collection_6',
+    ));
+    
+    $imgs_9 = elessi_import_upload('/wp-content/uploads/2016/06/watches_dark_store_location.jpg', '3117', array(
+        'post_title' => 'watches_dark_store_location',
+        'post_name' => 'watches_dark_store_location',
+    ));
+    
+    $imgs_10 = elessi_import_upload('/wp-content/uploads/2016/06/watches_dark_customer_service.jpg', '3117', array(
+        'post_title' => 'watches_dark_customer_service',
+        'post_name' => 'watches_dark_customer_service',
+    ));
+    
+    return array(
+        'post' => array(
+            'post_title' => 'WPB Watch Dark',
+            'post_name' => 'wpb-watch-dark',
+            'post_content' => '[vc_row fullwidth="1"][vc_column][nasa_rev_slider alias="slider-watches-dark"][/vc_column][/vc_row][vc_row equal_height="yes" width_side="left" el_class="margin-top-80 margin-bottom-80"][vc_column width="1/2" hide_in_mobile="1"][nasa_image image="3471"][/vc_column][vc_column width="1/2" el_class="right desktop-padding-left-40"][nasa_products number="4" columns_number="2" columns_number_tablet="2"][/vc_column][/vc_row][vc_row][vc_column width="1/2"][nasa_banner align="right" valign="middle" text-align="text-center" hover="zoom" img_src="' . $imgs_1 . '"]
+<h4 style="font-weight: 800; letter-spacing: 3px; margin-bottom: 20px;"><a style="color: #cccccc;" href="#">BIG SALE</a></h4>
+<h5 style="font-weight: 800; letter-spacing: 5px; color: #cccccc; font-size: 130%;">UP TO</h5>
+<h2 style="color: #e0b17a; font-weight: 800; line-height: 1.5;">30%</h2>
+[/nasa_banner][/vc_column][vc_column width="1/2"][nasa_banner valign="middle" text-align="text-center" hover="zoom" img_src="' . $imgs_2 . '"]
+<h4 style="line-height: 1.2; color: #e0b17a; font-weight: 800; letter-spacing: 5px;">BLACK</h4>
+<h4 style="line-height: 1.2; color: #e0b17a; font-weight: 800; letter-spacing: 5px;">EDITION</h4>
+<h5 style="text-decoration: underline;"><a style="font-size: 70%; font-weight: bold; color: #cccccc; letter-spacing: 3px;" title="Shop now" href="#">SHOP NOW</a></h5>
+[/nasa_banner][/vc_column][/vc_row][vc_row equal_height="yes" width_side="right" el_class="margin-top-80 margin-bottom-30"][vc_column width="1/2" el_class="desktop-padding-right-40 padding-left-10"][nasa_products number="4" columns_number="2" columns_number_tablet="2"][/vc_column][vc_column width="1/2" hide_in_mobile="1"][nasa_image image="3474"][/vc_column][/vc_row][vc_row][vc_column][nasa_title title_text="OUR COLLECTION" title_color="#e0b17a" title_type="h3" font_size="xl" title_align="text-center"][/vc_column][/vc_row][vc_row el_class="margin-top-30"][vc_column width="1/3"][nasa_banner hover="zoom" img_src="' . $imgs_3 . '" el_class="nasa-radius-10"][/nasa_banner][vc_column_text]
+<h5 class="primary-color text-center">CHRONOMAT</h5>
+[/vc_column_text][/vc_column][vc_column width="1/3" el_class="mobile-margin-top-30"][nasa_banner hover="zoom" img_src="' . $imgs_4 . '" el_class="nasa-radius-10"][/nasa_banner][vc_column_text]
+<h5 class="primary-color text-center">PROFESSIONAL</h5>
+[/vc_column_text][/vc_column][vc_column width="1/3" el_class="mobile-margin-top-30"][nasa_banner hover="zoom" img_src="' . $imgs_5 . '" el_class="nasa-radius-10"][/nasa_banner][vc_column_text]
+<h5 class="primary-color text-center">NAVITIMER</h5>
+[/vc_column_text][/vc_column][/vc_row][vc_row el_class="margin-top-30 margin-bottom-50"][vc_column width="1/3"][nasa_banner hover="zoom" img_src="' . $imgs_6 . '" el_class="nasa-radius-10"][/nasa_banner][vc_column_text]
+<h5 class="primary-color text-center">SUPEROCESIAN</h5>
+[/vc_column_text][/vc_column][vc_column width="1/3" el_class="mobile-margin-top-30"][nasa_banner hover="zoom" img_src="' . $imgs_7 . '" el_class="nasa-radius-10"][/nasa_banner][vc_column_text]
+<h5 class="primary-color text-center">PREMIER</h5>
+[/vc_column_text][/vc_column][vc_column width="1/3" el_class="mobile-margin-top-30"][nasa_banner hover="zoom" img_src="' . $imgs_8 . '" el_class="nasa-radius-10"][/nasa_banner][vc_column_text]
+<h5 class="primary-color text-center">AVIATOR</h5>
+[/vc_column_text][/vc_column][/vc_row][vc_row el_class="margin-bottom-50 margin-top-30"][vc_column width="1/3"][nasa_title title_text="TOP RATED" title_color="#e0b17a" title_type="h3" font_size="m" el_class="margin-bottom-20"][nasa_products type="featured_product" style="list" number="3" columns_number="1" columns_number_tablet="1" columns_number_small="1"][/vc_column][vc_column width="1/3" el_class="mobile-margin-top-50"][nasa_title title_text="BEST SELLER" title_color="#e0b17a" title_type="h3" font_size="m" el_class="margin-bottom-20"][nasa_products style="list" number="3" columns_number="1" columns_number_tablet="1" columns_number_small="1"][/vc_column][vc_column width="1/3" el_class="mobile-margin-top-50"][nasa_title title_text="ON SALE" title_color="#e0b17a" title_type="h3" font_size="m" el_class="margin-bottom-20"][nasa_products style="list" number="3" columns_number="1" columns_number_tablet="1" columns_number_small="1"][/vc_column][/vc_row][vc_row][vc_column width="1/2"][nasa_banner align="center" valign="middle" text-align="text-center" img_src="' . $imgs_9 . '"]
+<h4 style="font-weight: bold; letter-spacing: 5px; color: #fff;">STORE LOCATION</h4>
+<a class="button nasa-button-banner margin-top-20" style="border-radius: 99px; font-weight: bold; letter-spacing: 3px; color: #000000;" title="Shop now" href="#">FIND STORE</a>[/nasa_banner][/vc_column][vc_column width="1/2"][nasa_banner align="center" valign="middle" text-align="text-center" img_src="' . $imgs_10 . '"]
+<h4 style="font-weight: 800; letter-spacing: 5px; color: #fff;">CUSTOM SERVICE</h4>
+<a class="button nasa-button-banner margin-top-20" style="border-radius: 99px; font-weight: bold; letter-spacing: 3px; color: #000000;" title="Shop now" href="#">ALL WOMAN WATCHES</a>[/nasa_banner][/vc_column][/vc_row][vc_row el_class="padding-top-50 margin-bottom-50"][vc_column][nasa_brands images="' . elessi_imp_brands_dark_str() . '" columns_number="6" columns_number_tablet="4" columns_number_small="3" custom_links="#,#,#,#,#,#,#"][/vc_column][/vc_row]'
+        ),
+        
+        'post_meta' => array(
+            // '_nasa_custom_header' => '1',
+            '_nasa_header_transparent' => '1',
+            // '_nasa_site_bg_dark' => '1',
+            // '_nasa_pri_color_flag' => 'on',
+            // '_nasa_pri_color' => '#e0b17a',
+            // '_nasa_footer_mode' => 'builder',
+            // '_nasa_custom_footer' => 'footer-light-3',
+            // '_nasa_custom_footer_mobile' => 'footer-light-3'
+        ),
+        
+        'globals' => array(
+            'header-type' => '1',
+            
+            'site_bg_dark' => '1',
+            
+            // 'fixed_nav' => '0',
+            
+            // 'plus_wide_width' => '400',
+            'color_primary' => '#e0b17a',
+            
+            // 'bg_color_topbar' => '28aeb1',
+            // 'text_color_topbar' => '#ffffff',
+            
+            // 'fullwidth_main_menu' => '1',
+            
+            // 'bg_color_main_menu' => '#e4272c',
+            // 'text_color_main_menu' => '#ffffff',
+            
+            // 'v_root' => '1',
+            // 'v_root_limit' => '10',
+            
+            // 'bg_color_v_menu' => '#000000',
+            // 'text_color_v_menu' => '#ffffff',
+            
+            'footer_mode' => 'builder',
+            'footer-type' => 'footer-light-3',
+            'footer-mobile' => 'footer-light-3',
+            
+            // 'category_sidebar' => 'top-2',
+            'product_sidebar' => 'no',
+            
+            // 'product_detail_layout' => 'full',
+            // 'full_info_col' => '2',
+            'product_image_layout' => 'single',
+            // 'product_image_style' => 'slide',
+            // 'sp_bgl' => '#f6f6f6',
+            // 'tab_style_info' => 'scroll-down',
+            
+            // 'single_product_thumbs_style' => 'hoz',
+            
+            'loop_layout_buttons' => 'hoz-buttons',
+            
+            // 'animated_products' => 'hover-zoom',
+            
+            // 'nasa_attr_image_style' => 'square',
+            // 'nasa_attr_image_single_style' => 'square-caption',
+            // 'nasa_attr_color_style' => 'round',
+            // 'nasa_attr_label_style' => 'round',
+            
+            // 'breadcrumb_row' => 'single',
+            // 'breadcrumb_type' => 'default',
+            // 'breadcrumb_bg_color' => '#f8f8f8',
+            // 'breadcrumb_align' => 'text-left',
+            // 'breadcrumb_height' => '60',
+        ),
+    );
+}
