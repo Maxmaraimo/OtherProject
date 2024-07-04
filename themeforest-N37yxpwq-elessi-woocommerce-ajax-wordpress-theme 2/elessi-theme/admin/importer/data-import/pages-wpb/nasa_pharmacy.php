cb7a9c0b2789a70b3508d77bd3f3d0635cea8536
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-07-04 16:25:37 +0500

    12

---
+++ themeforest-N37yxpwq-elessi-woocommerce-ajax-wordpress-theme 2/elessi-theme/admin/importer/data-import/pages-wpb/nasa_pharmacy.php
@@ -0,0 +1,207 @@
+<?php
+function nasa_wpb_pharmacy() {
+    $imgs_1 = elessi_import_upload('/wp-content/uploads/2021/11/pharmacy-banner-111.jpg', '3659', array(
+        'post_title' => 'pharmacy-banner-111',
+        'post_name' => 'pharmacy-banner-111',
+    ));
+    
+    $imgs_2 = elessi_import_upload('/wp-content/uploads/2021/11/pharmacy-banner-222.jpg', '3658', array(
+        'post_title' => 'pharmacy-banner-222',
+        'post_name' => 'pharmacy-banner-222',
+    ));
+    
+    $imgs_3 = elessi_import_upload('/wp-content/uploads/2016/06/pharmacy-banner-33.jpg', '3658', array(
+        'post_title' => 'pharmacy-banner-33',
+        'post_name' => 'pharmacy-banner-33',
+    ));
+    
+    $imgs_4 = elessi_import_upload('/wp-content/uploads/2016/06/pharmacy-banner-44.jpg', '3660', array(
+        'post_title' => 'pharmacy-banner-44',
+        'post_name' => 'pharmacy-banner-44',
+    ));
+    
+    $imgs_5 = elessi_import_upload('/wp-content/uploads/2021/11/pharmacy-h-banner11.jpg', '3657', array(
+        'post_title' => 'pharmacy-h-banner11',
+        'post_name' => 'pharmacy-h-banner11',
+    ));
+    
+    $imgs_6 = elessi_import_upload('/wp-content/uploads/2016/06/pharmacy-banner-55.jpg', '3117', array(
+        'post_title' => 'pharmacy-banner-55',
+        'post_name' => 'pharmacy-banner-55',
+    ));
+    
+    $imgs_7 = elessi_import_upload('/wp-content/uploads/2016/06/pharmacy-banner-66.jpg', '3117', array(
+        'post_title' => 'pharmacy-banner-66',
+        'post_name' => 'pharmacy-banner-66',
+    ));
+    
+    $imgs_8 = elessi_import_upload('/wp-content/uploads/2016/06/pharmacy-banner-77.jpg', '3117', array(
+        'post_title' => 'pharmacy-banner-77',
+        'post_name' => 'pharmacy-banner-77',
+    ));
+    
+    $imgs_9 = elessi_import_upload('/wp-content/uploads/2021/11/pharmacy-h-banner-2-1.jpg', '3657', array(
+        'post_title' => 'pharmacy-h-banner-2-1',
+        'post_name' => 'pharmacy-h-banner-2-1',
+    ));
+    
+    $icon_1 = elessi_import_upload('/wp-content/uploads/2021/11/pharmacy-icon-1.png', '3074', array(
+        'post_title' => 'pharmacy-icon-1',
+        'post_name' => 'pharmacy-icon-1',
+    ));
+    
+    $icon_2 = elessi_import_upload('/wp-content/uploads/2021/11/pharmacy-icon-2.png', '3074', array(
+        'post_title' => 'pharmacy-icon-2',
+        'post_name' => 'pharmacy-icon-2',
+    ));
+    
+    $icon_3 = elessi_import_upload('/wp-content/uploads/2021/11/pharmacy-icon-3.png', '3074', array(
+        'post_title' => 'pharmacy-icon-3',
+        'post_name' => 'pharmacy-icon-3',
+    ));
+    
+    $icon_4 = elessi_import_upload('/wp-content/uploads/2021/11/pharmacy-icon-4.png', '3074', array(
+        'post_title' => 'pharmacy-icon-4',
+        'post_name' => 'pharmacy-icon-4',
+    ));
+    
+    $icon_5 = elessi_import_upload('/wp-content/uploads/2021/11/pharmacy-icon-5.png', '3074', array(
+        'post_title' => 'pharmacy-icon-5',
+        'post_name' => 'pharmacy-icon-5',
+    ));
+    
+    $icon_6 = elessi_import_upload('/wp-content/uploads/2021/11/pharmacy-icon-6.png', '3074', array(
+        'post_title' => 'pharmacy-icon-6',
+        'post_name' => 'pharmacy-icon-6',
+    ));
+    
+    $icon_7 = elessi_import_upload('/wp-content/uploads/2021/11/pharmacy-icon-7.png', '3074', array(
+        'post_title' => 'pharmacy-icon-7',
+        'post_name' => 'pharmacy-icon-7',
+    ));
+    
+    $icon_8 = elessi_import_upload('/wp-content/uploads/2021/11/pharmacy-icon-8.png', '3074', array(
+        'post_title' => 'pharmacy-icon-8',
+        'post_name' => 'pharmacy-icon-8',
+    ));
+    
+    $icon_9 = elessi_import_upload('/wp-content/uploads/2021/11/pharmacy-icon-9.png', '3074', array(
+        'post_title' => 'pharmacy-icon-9',
+        'post_name' => 'pharmacy-icon-9',
+    ));
+    
+    $icon_10 = elessi_import_upload('/wp-content/uploads/2021/11/pharmacy-icon-12.png', '3074', array(
+        'post_title' => 'pharmacy-icon-12',
+        'post_name' => 'pharmacy-icon-12',
+    ));
+    
+    return array(
+        'post' => array(
+            'post_title' => 'WPB Pharmacy',
+            'post_name' => 'wpb-pharmacy',
+            'post_content' => '[vc_row el_class="margin-top-20"][vc_column width="5/12"][nasa_banner_2 effect_text="fadeInLeft" data_delay="300ms" hover="zoom" img_src="' . $imgs_1 . '" el_class="nasa-radius-5 margin-bottom-0 mobile-margin-bottom-10"]
+<h3 class="fs-14 mobile-fs-15 desktop-margin-top-0 desktop-margin-bottom-15 primary-color" style="letter-spacing: 1px;">WEEKEND PROMOTION</h3>
+<h2 class="fs-35 tablet-fs-25 mobile-fs-20 margin-bottom-20" style="line-height: 1.2; color: #333333;"><span style="font-weight: 300;">Nutritional</span>
+Shake Mix Rose</h2>
+<p class="fs-16 tablet-fs-15 mobile-fs-15 margin-bottom-10" style="color: #666;">Makes a great tasting shake,
+formulated with all necessary quality protein.</p>
+[/nasa_banner_2][/vc_column][vc_column width="1/3"][nasa_banner_2 effect_text="fadeInLeft" data_delay="300ms" hover="zoom" img_src="' . $imgs_2 . '" el_class="nasa-radius-5 mobile-margin-bottom-10"]
+<h3 class="fs-14 margin-bottom-15 tablet-margin-bottom-10 primary-color" style="letter-spacing: 1px;">SPECIAL OFFER</h3>
+<h4 class="fs-22 tablet-fs-16 mobile-fs-18 margin-bottom-15 tablet-margin-bottom-0 mobile-margin-bottom-10" style="line-height: 1.2;">Health
+Essentials</h4>
+<h5 class="fs-14 hide-for-small hide-for-medium" style="color: #979797; font-weight: normal !important;">Discount</h5>
+<h4 class="hide-for-medium" style="color: #e1663e;">50%</h4>
+[/nasa_banner_2][nasa_banner_2 effect_text="fadeInLeft" data_delay="300ms" hover="zoom" img_src="' . $imgs_3 . '" el_class="nasa-radius-5 margin-bottom-0 mobile-margin-bottom-10"]
+<h3 class="fs-14 margin-bottom-15 tablet-margin-bottom-10 primary-color" style="letter-spacing: 1px;">LIMITED EDITION</h3>
+<h4 class="fs-22 tablet-fs-16 mobile-fs-18 margin-bottom-15 tablet-margin-bottom-0 mobile-margin-bottom-0" style="line-height: 1.2;">Blood
+Pressure
+Monitor</h4>
+<h5 class="fs-15 hide-for-small hide-for-medium" style="color: #979797; font-weight: normal !important;">Sale Off   <span class="fs-25 nasa-bold" style="color: #e1663e;">30%</span></h5>
+[/nasa_banner_2][/vc_column][vc_column width="1/4"][nasa_banner_2 move_x="12%" effect_text="fadeInLeft" data_delay="300ms" hover="zoom" img_src="' . $imgs_4 . '" el_class="nasa-radius-5 margin-bottom-0"]
+<h3 class="fs-14 margin-top-0 margin-bottom-25 tablet-margin-bottom-10 primary-color" style="letter-spacing: 1px;">SPECIAL OFFER</h3>
+<h4 class="fs-22 tablet-fs-16 mobile-fs-20 margin-bottom-25 tablet-margin-bottom-10" style="line-height: 1.2; color: #505050;">Place your first
+Order and get
+25% Off on Medicines</h4>
+<a class="primary-color button small fs-14 tablet-fs-11 force-radius-5" style="text-transform: capitalize; letter-spacing: 0;" title="Shop now" href="#">Shop Now</a>[/nasa_banner_2][/vc_column][/vc_row][vc_row el_class="bgw margin-top-20"][vc_column el_class="padding-top-15 padding-bottom-15"][nasa_title title_text="Shop by Categories" font_size="m" el_class="margin-bottom-10"][nasa_product_categories list_cats="men, woman, haircare-woman, makeup-woman, nails-woman, bracelets-woman, earrings-woman, necklaces-woman" parent="false" hide_empty="0" columns_number="7" columns_number_small="2" columns_number_tablet="3" auto_slide="false"][/vc_column][/vc_row][vc_row el_class="bgw margin-top-20"][vc_column el_class="padding-top-25"][vc_tta_tabs alignment="right" title_font_size="m" tabs_display_type="2d-has-bg" tabs_bg_color="" title="Popular Healthcare Products" el_class="letter-spacing-none"][vc_tta_section title="Ayush" tab_id="1596708228738-17953d91-53db"][nasa_products style="carousel" pos_nav="both" shop_url="0" arrows="1" number="7" columns_number="6" columns_number_tablet="3"][/vc_tta_section][vc_tta_section title="Mum &amp; Baby" tab_id="1596708230330-4a2f8401-99c3"][nasa_products type="featured_product" style="carousel" pos_nav="both" shop_url="1" arrows="1" auto_delay_time="7" columns_number="6" columns_number_tablet="3"][/vc_tta_section][vc_tta_section title="Vitamins &amp; Supplements" tab_id="1596708231252-34e7b24b-5d0f"][nasa_products type="on_sale" style="carousel" pos_nav="both" shop_url="1" arrows="1" number="7" columns_number="6" columns_number_tablet="3"][/vc_tta_section][/vc_tta_tabs][/vc_column][/vc_row][vc_row el_class="bgw margin-top-20"][vc_column el_class="padding-top-25 padding-bottom-40"][nasa_image_box_grid title="Shop by Health Concerns" glb_link="#" column_number="5" column_number_small="2" column_number_tablet="4"][nasa_image_box alt="Cardiac" link_text="#" image="' . $icon_1 . '"][nasa_image_box alt="Respiratory" link_text="#" image="' . $icon_2 . '"][nasa_image_box alt="Coughing" link_text="#" image="' . $icon_3 . '"][nasa_image_box alt="Sneeze" link_text="#" image="' . $icon_4 . '"][nasa_image_box alt="Stomach" link_text="#" image="' . $icon_5 . '"][nasa_image_box alt="Runny Noise" link_text="#" image="' . $icon_6 . '"][nasa_image_box alt="Sick" link_text="#" image="' . $icon_7 . '"][nasa_image_box alt="Tooth Cracked" link_text="#" image="' . $icon_8 . '"][nasa_image_box alt="Fever" link_text="#" image="' . $icon_9 . '"][nasa_image_box alt="Headache" link_text="#" image="' . $icon_10 . '"][/nasa_image_box_grid][/vc_column][/vc_row][vc_row hide_in_mobile="1" el_class="hide-for-medium hide-for-small margin-top-20"][vc_column][nasa_banner_2 align="center" valign="middle" text_align="text-center" hide_in_m="1" img_src="' . $imgs_5 . '" el_class="margin-bottom-0"]
+<p class="fs-16 color-white text-center margin-bottom-0 nasa-flex rtl-je align-baseline nasa-relative" style="padding-left: 18%;">Save flat <span class="fs-25">25%</span> extra on medicines &amp; enjoy <b>FREE</b> delivery with PLUS membership
+<a class="button small fs-14 force-radius-5 vertical-align" style="text-transform: none; letter-spacing: 0; position: absolute; right: 40px; background-color: #fed700; color: #000000; padding: 8px 10px;" title="View more" href="#">View more</a></p>
+[/nasa_banner_2][/vc_column][/vc_row][vc_row el_class="bgw margin-top-20"][vc_column el_class="padding-top-25"][vc_tta_tabs alignment="right" title_font_size="m" tabs_display_type="2d-has-bg" tabs_bg_color="" title="Recommended For You" el_class="letter-spacing-none"][vc_tta_section title="Covid Essentials" tab_id="1636795339712-7f000004-6c11"][nasa_products style="carousel" pos_nav="both" shop_url="1" arrows="1" number="7" columns_number="6" columns_number_tablet="3"][/vc_tta_section][vc_tta_section title="Nutrition" tab_id="1636795339735-4b379af2-47b9"][nasa_products type="featured_product" style="carousel" pos_nav="both" shop_url="1" arrows="1" number="7" columns_number="6" columns_number_tablet="3"][/vc_tta_section][vc_tta_section title="Beauty" tab_id="1636795339755-8ecd355a-ab9b"][nasa_products style="carousel" pos_nav="both" shop_url="1" arrows="1" number="7" columns_number="6" columns_number_tablet="3"][/vc_tta_section][/vc_tta_tabs][/vc_column][/vc_row][vc_row el_class="margin-top-20"][vc_column width="1/3" offset="vc_col-md-4"][nasa_banner_2 move_x="6%" valign="middle" effect_text="fadeInLeft" hover="zoom" img_src="' . $imgs_6 . '" el_class="nasa-radius-5 margin-bottom-0 mobile-margin-bottom-10"]
+<h3 class="fs-25 tablet-fs-16 mobile-fs-20 margin-bottom-15 tablet-margin-bottom-10 mobile-margin-bottom-20" style="line-height: 1.2;"><span style="font-weight: 300 !important;">Collagen</span>
+&amp; Perfomance</h3>
+<a class="primary-color button small fs-14 tablet-fs-12 force-radius-5" style="text-transform: capitalize; letter-spacing: 0;" title="Shop now" href="#">Shop Now</a>[/nasa_banner_2][/vc_column][vc_column width="1/3" offset="vc_col-md-4"][nasa_banner_2 effect_text="flipInX" data_delay="600ms" hover="zoom" img_src="' . $imgs_7 . '" el_class="nasa-radius-5 margin-bottom-0 mobile-margin-bottom-10"]
+<h4 class="margin-bottom-5 tablet-margin-bottom-0 mobile-margin-bottom-10 fs-25 tablet-fs-16 mobile-fs-20" style="line-height: 1.2;">Skin care
+Nutrient <span style="font-weight: 300! important!;">Rangle </span></h4>
+<h6 class="margin-bottom-20 tablet-margin-bottom-10 mobile-margin-bottom-15 fs-16 tablet-fs-14 mobile-fs-14" style="line-height: 1.2;">Sale Off <span class="fs-30 tablet-fs-16 mobile-fs-18" style="font-weight: 500;">30%</span></h6>
+<a class="primary-color hide-for-medium button small fs-14 tablet-fs-12 force-radius-5" style="text-transform: capitalize; letter-spacing: 0;" title="Shop now" href="#">Shop Now</a>[/nasa_banner_2][/vc_column][vc_column width="1/3" offset="vc_col-md-4"][nasa_banner_2 valign="middle" effect_text="fadeInUp" data_delay="600ms" hover="zoom" img_src="' . $imgs_8 . '" el_class="nasa-radius-5 margin-bottom-0"]
+<h4 class="fs-25 tablet-fs-16 mobile-fs-20 margin-top-30 margin-bottom-20 tablet-margin-bottom-10" style="line-height: 1.2; color: #505050;">Organic
+<span style="font-weight: 300! important!;">&amp; Gluten Free</span></h4>
+<a class="primary-color button small fs-14 tablet-fs-12 force-radius-5" style="text-transform: capitalize; letter-spacing: 0;" title="Shop now" href="#">Shop Now</a>[/nasa_banner_2][/vc_column][/vc_row][vc_row el_class="bgw margin-top-20"][vc_column el_class="padding-top-25"][vc_tta_tabs alignment="right" title_font_size="m" tabs_display_type="2d-has-bg" tabs_bg_color="" title="Picked By Elessi" el_class="letter-spacing-none"][vc_tta_section title="Latest Products" tab_id="1637461889477-f40b3934-9825"][nasa_products style="carousel" pos_nav="both" shop_url="1" arrows="1" number="7" columns_number="6" columns_number_tablet="3"][/vc_tta_section][vc_tta_section title="Best Seller" tab_id="1637461889518-36203949-d0b6"][nasa_products type="best_selling" style="carousel" pos_nav="both" shop_url="1" arrows="1" number="7" columns_number="6" columns_number_tablet="3"][/vc_tta_section][vc_tta_section title="Featured" tab_id="1637461889567-711030cd-b26e"][nasa_products type="featured_product" style="carousel" pos_nav="both" shop_url="1" arrows="1" number="7" columns_number="6" columns_number_tablet="3"][/vc_tta_section][/vc_tta_tabs][/vc_column][/vc_row][vc_row hide_in_mobile="1" el_class="hide-for-medium hide-for-small margin-top-20"][vc_column][nasa_banner_2 align="center" valign="middle" text_align="text-center" hide_in_m="1" img_src="' . $imgs_9 . '" el_class="margin-bottom-0"]
+<p class="fs-16 text-center margin-bottom-0 nasa-flex rtl-je align-baseline nasa-relative" style="padding-left: 15%;"><b>Order medicines</b> and get same-day delivery. <b>100% Secure delivery</b> without contacting the courier
+<a class="button small fs-14 force-radius-5 vertical-align" style="text-transform: none; letter-spacing: 0; position: absolute; right: 40px; padding: 8px 15px;" title="Order Now" href="#">Order Now</a></p>
+[/nasa_banner_2][/vc_column][/vc_row][vc_row el_class="bgw margin-top-20"][vc_column width="1/3" el_class="padding-top-25 padding-bottom-25 mobile-padding-top-45 mobile-padding-bottom-0"][nasa_products title_shortcode="Newest" style="list_carousel" style_row="3" arrows="1" number="6" columns_number="1" columns_number_tablet="1" columns_number_small="1"][/vc_column][vc_column width="1/3" el_class="padding-top-25 padding-bottom-25 mobile-padding-bottom-0"][nasa_products title_shortcode="Best Selling" type="best_selling" style="list_carousel" style_row="3" arrows="1" number="6" columns_number="1" columns_number_tablet="1" columns_number_small="1"][/vc_column][vc_column width="1/3" el_class="padding-top-25 padding-bottom-25"][nasa_products title_shortcode="On Sale" style="list_carousel" style_row="3" arrows="1" number="6" columns_number="1" columns_number_tablet="1" columns_number_small="1"][/vc_column][/vc_row][vc_row el_class="margin-top-20"][vc_column el_class="padding-top-25 padding-bottom-25"][nasa_title title_text="Advice From Health Experts" title_desc="The freshest and most exciting news" font_size="m" el_class="margin-bottom-30"][nasa_post dots="true" arrows="1" posts="6" columns_number="5" columns_number_small_slider="1" columns_number_tablet="2" readmore="no" page_blogs="no"][/vc_column][/vc_row]'
+        ),
+        'post_meta' => array(
+            // '_nasa_custom_header' => '4',
+            '_nasa_site_bg_dark' => '2',
+            // '_nasa_plus_wide_option' => '1',
+            // '_nasa_plus_wide_width' => '400',
+            // '_nasa_pri_color_flag' => 'on',
+            // '_nasa_pri_color' => '#28b2a9',
+            // '_nasa_footer_mode' => 'builder',
+            // '_nasa_custom_footer' => 'footer-light-1',
+        ),
+        
+        'globals' => array(
+            'header-type' => '4',
+            
+            'fixed_nav' => '0',
+            
+            'plus_wide_width' => '400',
+            'color_primary' => '#28b2a9',
+            
+            // 'bg_color_topbar' => '28aeb1',
+            // 'text_color_topbar' => '#ffffff',
+            
+            // 'fullwidth_main_menu' => '1',
+            
+            // 'bg_color_main_menu' => '#e4272c',
+            // 'text_color_main_menu' => '#ffffff',
+            
+            // 'v_root' => '1',
+            // 'v_root_limit' => '10',
+            
+            // 'bg_color_v_menu' => '#000000',
+            // 'text_color_v_menu' => '#ffffff',
+            
+            'footer_mode' => 'builder',
+            'footer-type' => 'footer-light-1',
+            'footer-mobile' => 'footer-mobile',
+            
+            'category_sidebar' => 'left-classic',
+            'wrapper_widgets' => '1',
+            
+            'product_detail_layout' => 'classic',
+            // 'product_image_layout' => 'single',
+            // 'product_image_style' => 'slide',
+            // 'sp_bgl' => '#f6f6f6',
+            // 'tab_style_info' => '2d-no-border',
+            
+            'single_product_thumbs_style' => 'hoz',
+            
+            'loop_layout_buttons' => 'hoz-buttons',
+            
+            // 'animated_products' => 'hover-carousel',
+            
+            // 'nasa_attr_image_style' => 'square',
+            // 'nasa_attr_image_single_style' => 'extends',
+            // 'nasa_attr_color_style' => 'round',
+            // 'nasa_attr_label_style' => 'round',
+            
+            'breadcrumb_row' => 'single',
+            'breadcrumb_type' => 'default',
+            'breadcrumb_bg_color' => '#f8f8f8',
+            'breadcrumb_align' => 'text-left',
+            'breadcrumb_height' => '60',
+        ),
+    );
+}
