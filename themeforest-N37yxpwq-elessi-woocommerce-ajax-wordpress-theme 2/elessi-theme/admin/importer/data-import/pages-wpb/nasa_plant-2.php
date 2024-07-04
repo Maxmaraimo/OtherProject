5176987876729a993dc3430568873a68ecb2507d
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-07-04 16:27:43 +0500

    13

---
+++ themeforest-N37yxpwq-elessi-woocommerce-ajax-wordpress-theme 2/elessi-theme/admin/importer/data-import/pages-wpb/nasa_plant-2.php
@@ -0,0 +1,120 @@
+<?php
+function nasa_wpb_plant_2() {
+    $imgs_1 = elessi_import_upload('/elementor/wp-content/uploads/2022/07/plant2-banner1.jpg', '3136', array(
+        'post_title' => 'plant2-banner1',
+        'post_name' => 'plant2-banner1'
+    ));
+    
+    $imgs_2 = elessi_import_upload('/elementor/wp-content/uploads/2022/07/plant2-banner2.jpg', '3136', array(
+        'post_title' => 'plant2-banner2',
+        'post_name' => 'plant2-banner2'
+    ));
+    
+    $imgs_3 = elessi_import_upload('/elementor/wp-content/uploads/2022/07/plant2-banner3.jpg', '3136', array(
+        'post_title' => 'plant2-banner3',
+        'post_name' => 'plant2-banner3'
+    ));
+    
+    $imgs_4 = elessi_import_upload('/elementor/wp-content/uploads/2022/07/plant2-d.jpg', '3174', array(
+        'post_title' => 'plant2-d',
+        'post_name' => 'plant2-d'
+    ));
+    
+    $imgs_5 = elessi_import_upload('/elementor/wp-content/uploads/2022/07/plant2-icon1.png', '3149', array(
+        'post_title' => 'plant2-icon1',
+        'post_name' => 'plant2-icon1'
+    ));
+    
+    $imgs_6 = elessi_import_upload('/elementor/wp-content/uploads/2022/07/plant2-icon2.png', '3149', array(
+        'post_title' => 'plant2-icon2',
+        'post_name' => 'plant2-icon2'
+    ));
+    
+    $imgs_7 = elessi_import_upload('/elementor/wp-content/uploads/2022/07/plant2-icon3.png', '3149', array(
+        'post_title' => 'plant2-icon3',
+        'post_name' => 'plant2-icon3'
+    ));
+    
+    return array(
+        'post' => array(
+            'post_title' => 'WPB Plant V2',
+            'post_name' => 'wpb-plant-v2',
+            'post_content' => '[vc_row fullwidth="1" el_class="margin-bottom-30 tablet-margin-bottom-15 mobile-margin-bottom-10"][vc_column][nasa_pin_products_banner pin_slug="pin-products-home-furniture" bg_icon="#ffffff" txt_color="#333333" border_icon="#6bac0a" el_class="text-center"][/vc_column][/vc_row][vc_row el_class="margin-bottom-60 tablet-margin-bottom-50 mobile-margin-bottom-40"][vc_column width="1/3"][nasa_banner_2 valign="middle" effect_text="fadeInLeft" data_delay="100ms" hover="zoom" img_src="' . $imgs_1 . '" el_class="nasa-bold-500"]
+<h3 class="fs-23 tablet-fs-25 mobile-fs-26 margin-bottom-25 tablet-margin-bottom-20" style="line-height: 1.2;">Plants
+For Interior</h3>
+<div class="fs-16 tablet-fs-23 mobile-fs-23" style="color: #78716b;">Sale off <span class="fs-28 tablet-fs-16 mobile-fs-15 primary-color">25%</span></div>
+[/nasa_banner_2][/vc_column][vc_column width="1/3"][nasa_banner_2 valign="middle" effect_text="fadeInLeft" data_delay="100ms" hover="zoom" img_src="' . $imgs_2 . '"]
+<h3 class="fs-23 tablet-fs-25 mobile-fs-26 margin-bottom-20 tablet-margin-bottom-10 mobile-margin-bottom-40" style="line-height: 1.2;"><span class="primary-color">Special</span>
+House
+Plants</h3>
+<a class="fs-14" style="text-decoration: underline; text-transform: uppercase;" title="Shop now" href="#">Shop now</a>[/nasa_banner_2][/vc_column][vc_column width="1/3"][nasa_banner_2 valign="middle" effect_text="fadeInLeft" data_delay="100ms" hover="zoom" img_src="' . $imgs_3 . '"]
+<h3 class="fs-23 tablet-fs-25 mobile-fs-26 margin-top-0 margin-bottom-20" style="line-height: 1.2;">Green
+Plants</h3>
+<a class="fs-14" style="text-decoration: underline; text-transform: uppercase;" title="Shop now" href="#">Shop now</a>[/nasa_banner_2][/vc_column][/vc_row][vc_row el_class="margin-bottom-50"][vc_column][vc_column_text el_class="text-center"]
+<p class="primary-color fs-16 nasa-bold-500 margin-bottom-0">NEW ARRIVALS</p>
+
+<h3 class="fs-30 tablet-fs-28 mobile-fs-27 margin-top-0 margin-bottom-10" style="color: #333333;">Recommended For You</h3>
+[/vc_column_text][vc_tta_tabs alignment="center" tabs_display_type="2d-radius-dashed"][vc_tta_section title="SMALL PLANTS" tab_id="1659518546031-6df5e6ba-7b43"][nasa_products columns_number="4" columns_number_tablet="3"][/vc_tta_section][vc_tta_section title="TERRARIUMS" tab_id="1659518546035-02a0c9dc-726e"][nasa_products type="best_selling" columns_number="4" columns_number_tablet="3"][/vc_tta_section][vc_tta_section title="HOUSE PLANTS" tab_id="1659518553850-e77e0626-2c90"][nasa_products type="featured_product" columns_number="4" columns_number_tablet="3"][/vc_tta_section][vc_tta_section title="SUCCULENTS" tab_id="1659518554981-e23eaa9c-f05b"][nasa_products columns_number="4" columns_number_tablet="3"][/vc_tta_section][/vc_tta_tabs][/vc_column][/vc_row][vc_row content_placement="middle" css=".vc_custom_1660382462941{background-color: #f8f8f8 !important;}" el_class="margin-bottom-60 desktop-padding-top-60 padding-bottom-60 mobile-padding-top-50 mobile-padding-bottom-30"][vc_column width="1/2" offset="vc_col-lg-6 vc_col-md-6"][nasa_image image="' . $imgs_4 . '" align="center"][/vc_column][vc_column width="5/12" offset="vc_col-lg-offset-1 vc_col-lg-5 vc_col-md-offset-0 vc_col-md-6 vc_col-sm-offset-0" el_class="tablet-margin-left-0"][vc_row_inner][vc_column_inner el_class="margin-top-35 margin-bottom-35"][vc_single_image image="' . $imgs_5 . '" img_size="full" alignment="center" el_class="left rtl-right margin-right-20 rtl-margin-left-20 rtl-margin-right-0 margin-top-0 nasa-radius-55 primary-bg" css=".vc_custom_1659604873513{padding-top: 20px !important;padding-right: 20px !important;padding-bottom: 20px !important;padding-left: 20px !important;}"][nasa_title title_text="Sercure Payment" title_desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry" el_class="nasa-clear-none nasa-flex flex-wrap"][/vc_column_inner][vc_column_inner el_class="margin-bottom-35"][vc_single_image image="' . $imgs_6 . '" img_size="full" alignment="center" el_class="left rtl-right margin-right-20 rtl-margin-left-20 rtl-margin-right-0 margin-top-0 nasa-radius-55 primary-bg" css=".vc_custom_1659604925552{padding-top: 20px !important;padding-right: 20px !important;padding-bottom: 20px !important;padding-left: 20px !important;}"][nasa_title title_text="On Time Delivery" title_desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry" el_class="nasa-clear-none nasa-flex flex-wrap"][/vc_column_inner][vc_column_inner el_class="margin-bottom-35"][vc_single_image image="' . $imgs_7 . '" alignment="center" el_class="left rtl-right margin-right-20 rtl-margin-left-20 rtl-margin-right-0 margin-top-0 nasa-radius-55 primary-bg" css=".vc_custom_1659605759576{padding-top: 20px !important;padding-right: 20px !important;padding-bottom: 20px !important;padding-left: 20px !important;}"][nasa_title title_text="24/7 Support" title_desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry" el_class="nasa-clear-none nasa-flex flex-wrap"][/vc_column_inner][/vc_row_inner][/vc_column][/vc_row][vc_row el_class="margin-bottom-50 mobile-margin-bottom-30"][vc_column width="1/3" el_class="mobile-margin-bottom-30"][nasa_title title_text="Top Rated" title_type="h4" font_size="m" el_class="margin-bottom-30"][nasa_products type="top_rate" style="list" number="4" columns_number="1" columns_number_tablet="1" columns_number_small="1"][/vc_column][vc_column width="1/3" el_class="mobile-margin-bottom-30"][nasa_title title_text="Best Selling" title_type="h4" font_size="m" el_class="margin-bottom-30"][nasa_products type="best_selling" style="list" number="4" columns_number="1" columns_number_tablet="1" columns_number_small="1"][/vc_column][vc_column width="1/3"][nasa_title title_text="On Sale" title_type="h4" font_size="m" el_class="margin-bottom-30"][nasa_products type="on_sale" style="list" number="4" columns_number="1" columns_number_tablet="1" columns_number_small="1"][/vc_column][/vc_row][vc_row el_class="margin-bottom-30 mobile-margin-bottom-0"][vc_column][nasa_post title="Latest Posts" title_desc="The freshest and most exciting news" show_type="grid_3" cats_enable="no" date_enable="no" author_enable="no" readmore="no" page_blogs="no"][/vc_column][/vc_row]'
+        ),
+        'post_meta' => array(
+            // '_nasa_pri_color_flag' => 'on',
+            // '_nasa_pri_color' => '#6bac0a',
+            // '_nasa_footer_mode' => 'builder',
+            // '_nasa_custom_footer' => 'wpb-footer-light-2-bg',
+            // '_nasa_custom_footer_mobile' => 'footer-mobile',
+            '_wpb_shortcodes_custom_css' => '.vc_custom_1660382462941{background-color: #f8f8f8 !important;}.vc_custom_1659604873513{padding-top: 20px !important;padding-right: 20px !important;padding-bottom: 20px !important;padding-left: 20px !important;}.vc_custom_1659604925552{padding-top: 20px !important;padding-right: 20px !important;padding-bottom: 20px !important;padding-left: 20px !important;}.vc_custom_1659605759576{padding-top: 20px !important;padding-right: 20px !important;padding-bottom: 20px !important;padding-left: 20px !important;}'
+        ),
+        
+        'globals' => array(
+            'header-type' => '1',
+            
+            // 'fixed_nav' => '0',
+            
+            // 'plus_wide_width' => '400',
+            'color_primary' => '#6bac0a',
+            
+            // 'bg_color_topbar' => '28aeb1',
+            // 'text_color_topbar' => '#ffffff',
+            
+            // 'fullwidth_main_menu' => '1',
+            
+            // 'bg_color_main_menu' => '#e4272c',
+            // 'text_color_main_menu' => '#ffffff',
+            
+            // 'v_root' => '1',
+            // 'v_root_limit' => '10',
+            
+            // 'bg_color_v_menu' => '#000000',
+            // 'text_color_v_menu' => '#ffffff',
+            
+            'footer_mode' => 'builder',
+            'footer-type' => 'wpb-footer-light-2-bg',
+            'footer-mobile' => 'footer-mobile',
+            
+            // 'category_sidebar' => 'left-classic',
+            
+            // 'product_detail_layout' => 'classic',
+            'product_image_layout' => 'single',
+            'product_image_style' => 'slide',
+            // 'sp_bgl' => '#f6f6f6',
+            // 'tab_style_info' => '2d-no-border',
+            
+            // 'single_product_thumbs_style' => 'hoz',
+            
+            'loop_layout_buttons' => 'modern-5',
+            
+            // 'animated_products' => 'hover-carousel',
+            
+            // 'nasa_attr_image_style' => 'square',
+            // 'nasa_attr_image_single_style' => 'extends',
+            // 'nasa_attr_color_style' => 'round',
+            'nasa_attr_label_style' => 'small-square-1',
+            
+            // 'breadcrumb_row' => 'single',
+            // 'breadcrumb_type' => 'default',
+            // 'breadcrumb_bg_color' => '#f8f8f8',
+            // 'breadcrumb_align' => 'text-left',
+            // 'breadcrumb_height' => '60',
+        ),
+    );
+}
