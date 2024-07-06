15e677e2c2713b758639679fc917e8a2c4ecfd93
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-07-06 12:21:18 +0500

    7

---
+++ themeforest-N37yxpwq-elessi-woocommerce-ajax-wordpress-theme 2/elessi-theme/headers/header-builder.php
@@ -0,0 +1,17 @@
+<?php defined('ABSPATH') or die(); // Exit if accessed directly ?>
+
+<div class="header-wrapper header-type-builder">
+    <?php
+    /**
+     * Hook - top bar header
+     */
+    // do_action('nasa_topbar_header');
+    
+    if (isset($header_builder) && $header_builder) : ?>
+        <div class="header-content-builder nasa-header-content-builder">
+            <div id="masthead" class="site-header">
+                <?php echo $header_builder; ?>
+            </div>
+        </div>
+    <?php endif; ?>
+</div>
