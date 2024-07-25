08be4d3d5b8dc1629c1ec27ba50da6d7839ddf1d
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-07-25 17:19:03 +0500

    15

---
+++ themeforest-N37yxpwq-elessi-woocommerce-ajax-wordpress-theme 2/elessi-theme/woocommerce/checkout/form-checkout.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Checkout Form
+ *
+ * @author  NasaTheme
+ * @package Elessi-theme/WooCommerce
+ * @version 3.5.0
+ */
+
+if (!defined('ABSPATH')) :
+    exit; // Exit if accessed directly
+endif;
+
+do_action('nasa_checkout_form_layout', $checkout);
