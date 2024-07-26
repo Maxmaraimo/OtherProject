b1ec4c6ed0e431ac5d29f90a22ed4e58eb9c4d53
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-07-26 12:31:19 +0500

    10

---
+++ themeforest-N37yxpwq-elessi-woocommerce-ajax-wordpress-theme 2/elessi-theme/woocommerce/myaccount/form-login.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Login Form
+ *
+ * @author  NasaTheme
+ * @package Elessi-theme/WooCommerce
+ * @version 7.0.1
+ */
+
+defined('ABSPATH') or exit; // Exit if accessed directly
+
+do_action('nasa_login_register_form');
