4678f50020e16fc4799acf5ce4c1342f6f1d6748
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-07-26 12:31:22 +0500

    11

---
+++ themeforest-N37yxpwq-elessi-woocommerce-ajax-wordpress-theme 2/elessi-theme/woocommerce/notices/notice.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Show messages
+ *
+ * @author  NasaTheme
+ * @package Elessi-theme/WooCommerce
+ * @version 8.6.0
+ */
+
+if (!defined('ABSPATH')) :
+    exit;
+endif;
+
+if (empty($notices) || !is_array($notices)) :
+    return;
+endif;
+?>
+
+<?php foreach ($notices as $notice) : ?>
+    <div class="woocommerce-info"<?php echo wc_get_notice_data_attr($notice); ?> role="alert">
+        <?php echo wc_kses_notice($notice['notice']); ?>
+    </div>
+<?php
+endforeach;
