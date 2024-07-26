92db2e710ac1f021bdaac67b84c6dafedd757e6f
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-07-26 12:30:54 +0500

    6

---
+++ themeforest-N37yxpwq-elessi-woocommerce-ajax-wordpress-theme 2/elessi-theme/woocommerce/loop/loop-end.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Product Loop End
+ *
+ * @author  NasaTheme
+ * @package Elessi-theme/WooCommerce
+ * @version 2.0.0
+ */
+
+if (!defined('ABSPATH')) :
+    exit; // Exit if accessed directly
+endif;
+?>
+</ul>
+</div>
